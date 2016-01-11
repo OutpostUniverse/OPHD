cef690e553662a440d5e52f3ec01de26766323ca
leeor_net <leeor_net@bdf9137d-f3be-7946-88fe-7b0bbfdbf31a>
2016-01-11 01:21:59 +0000

    - Converted ResourcePool from Double's to Int's. - Implemented storage management functions in ResourcePool. Implements IssueID #34.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outposthd@49 bdf9137d-f3be-7946-88fe-7b0bbfdbf31a

--- src/ResourcePool.cpp
+++ src/ResourcePool.cpp
@@ -1,10 +1,14 @@
 #include "ResourcePool.h"
 
+#include <iostream>
 
-ResourcePool::ResourcePool()
+using namespace std;
+
+
+ResourcePool::ResourcePool(): _capacity(0)
 {}
 
-ResourcePool::ResourcePool(const ResourcePool& _r) : _resourceTable(_r._resourceTable)
+ResourcePool::ResourcePool(const ResourcePool& _r) : _resourceTable(_r._resourceTable), _capacity(_r._capacity)
 {}
 
 
@@ -15,6 +19,8 @@ ResourcePool::~ResourcePool()
 ResourcePool& ResourcePool::operator=(const ResourcePool& rhs)
 {
 	_resourceTable = rhs._resourceTable;
+	_capacity = rhs._capacity;
+
 	return *this;
 }
 
@@ -30,6 +36,12 @@ void ResourcePool::clear()
 
 ResourcePool& ResourcePool::operator+=(ResourcePool& rhs)
 {
+	if (_capacity != 0)
+	{
+		cout << "ResourcePool::operator+=(): Incorrect use of operator. This ResourcePool has a capacity. Use pushResource() or pushResources() instead." << endl;
+		return *this;
+	}
+
 	_resourceTable[RESOURCE_COMMON_METALS_ORE] += rhs.commonMetalsOre();
 	_resourceTable[RESOURCE_COMMON_MINERALS_ORE] += rhs.commonMineralsOre();
 	_resourceTable[RESOURCE_RARE_METALS_ORE] += rhs.rareMetalsOre();
@@ -48,6 +60,12 @@ ResourcePool& ResourcePool::operator+=(ResourcePool& rhs)
 
 ResourcePool& ResourcePool::operator-=(ResourcePool& rhs)
 {
+	if (_capacity != 0)
+	{
+		cout << "ResourcePool::operator-=(): Incorrect use of operator. This ResourcePool has a capacity. Use pullResource() or pullResources() instead." << endl;
+		return *this;
+	}
+
 	_resourceTable[RESOURCE_COMMON_METALS_ORE] -= rhs.commonMetalsOre();
 	_resourceTable[RESOURCE_COMMON_MINERALS_ORE] -= rhs.commonMineralsOre();
 	_resourceTable[RESOURCE_RARE_METALS_ORE] -= rhs.rareMetalsOre();
@@ -65,46 +83,188 @@ ResourcePool& ResourcePool::operator-=(ResourcePool& rhs)
 }
 
 
-double ResourcePool::resource(ResourceType _t)
+int ResourcePool::resource(ResourceType _t)
 {
 	return _resourceTable[_t];
 }
 
 
-void ResourcePool::resource(ResourceType _t, double _d)
+void ResourcePool::resource(ResourceType _t, int _i)
 {
-	_resourceTable[_t] = _d;
+	_resourceTable[_t] = _i;
 }
 
 
-double ResourcePool::commonMetalsOre() { return resource(RESOURCE_COMMON_METALS_ORE); }
-double ResourcePool::commonMineralsOre() { return resource(RESOURCE_COMMON_MINERALS_ORE); }
-double ResourcePool::rareMetalsOre() { return resource(RESOURCE_RARE_METALS_ORE); }
-double ResourcePool::rareMineralsOre() { return resource(RESOURCE_RARE_MINERALS_ORE); }
+int ResourcePool::commonMetalsOre() { return resource(RESOURCE_COMMON_METALS_ORE); }
+int ResourcePool::commonMineralsOre() { return resource(RESOURCE_COMMON_MINERALS_ORE); }
+int ResourcePool::rareMetalsOre() { return resource(RESOURCE_RARE_METALS_ORE); }
+int ResourcePool::rareMineralsOre() { return resource(RESOURCE_RARE_MINERALS_ORE); }
 
-double ResourcePool::commonMetals() { return resource(RESOURCE_COMMON_METALS); }
-double ResourcePool::commonMinerals() { return resource(RESOURCE_COMMON_MINERALS); }
-double ResourcePool::rareMetals() { return resource(RESOURCE_RARE_METALS); }
-double ResourcePool::rareMinerals() { return resource(RESOURCE_RARE_MINERALS); }
+int ResourcePool::commonMetals() { return resource(RESOURCE_COMMON_METALS); }
+int ResourcePool::commonMinerals() { return resource(RESOURCE_COMMON_MINERALS); }
+int ResourcePool::rareMetals() { return resource(RESOURCE_RARE_METALS); }
+int ResourcePool::rareMinerals() { return resource(RESOURCE_RARE_MINERALS); }
 
-double ResourcePool::energy() { return resource(RESOURCE_FOOD); }
-double ResourcePool::food() { return resource(RESOURCE_ENERGY); }
+int ResourcePool::energy() { return resource(RESOURCE_FOOD); }
+int ResourcePool::food() { return resource(RESOURCE_ENERGY); }
 
-void ResourcePool::commonMetalsOre(double _d) { resource(RESOURCE_COMMON_METALS_ORE, _d); }
-void ResourcePool::commonMineralsOre(double _d) { resource(RESOURCE_COMMON_MINERALS_ORE, _d); }
-void ResourcePool::rareMetalsOre(double _d) { resource(RESOURCE_RARE_METALS_ORE, _d); }
-void ResourcePool::rareMineralsOre(double _d) { resource(RESOURCE_RARE_MINERALS_ORE, _d); }
+void ResourcePool::commonMetalsOre(int _i) { resource(RESOURCE_COMMON_METALS_ORE, _i); }
+void ResourcePool::commonMineralsOre(int _i) { resource(RESOURCE_COMMON_MINERALS_ORE, _i); }
+void ResourcePool::rareMetalsOre(int _i) { resource(RESOURCE_RARE_METALS_ORE, _i); }
+void ResourcePool::rareMineralsOre(int _i) { resource(RESOURCE_RARE_MINERALS_ORE, _i); }
 
-void ResourcePool::commonMetals(double _d) { resource(RESOURCE_COMMON_METALS, _d); }
-void ResourcePool::commonMinerals(double _d) { resource(RESOURCE_COMMON_MINERALS, _d); }
-void ResourcePool::rareMetals(double _d) { resource(RESOURCE_RARE_METALS, _d); }
-void ResourcePool::rareMinerals(double _d) { resource(RESOURCE_RARE_MINERALS, _d); }
+void ResourcePool::commonMetals(int _i) { resource(RESOURCE_COMMON_METALS, _i); }
+void ResourcePool::commonMinerals(int _i) { resource(RESOURCE_COMMON_MINERALS, _i); }
+void ResourcePool::rareMetals(int _i) { resource(RESOURCE_RARE_METALS, _i); }
+void ResourcePool::rareMinerals(int _i) { resource(RESOURCE_RARE_MINERALS, _i); }
 
-void ResourcePool::energy(double _d) { resource(RESOURCE_FOOD, _d); }
-void ResourcePool::food(double _d) { resource(RESOURCE_ENERGY, _d); }
+void ResourcePool::energy(int _i) { resource(RESOURCE_FOOD, _i); }
+void ResourcePool::food(int _i) { resource(RESOURCE_ENERGY, _i); }
 
 
+/**
+ * Returns the current amount of resources in the ResourcePool.
+ */
+int ResourcePool::currentLevel()
+{
+	int cc = resource(RESOURCE_COMMON_METALS_ORE);
+	cc += resource(RESOURCE_COMMON_MINERALS_ORE);
+	cc += resource(RESOURCE_RARE_METALS_ORE);
+	cc += resource(RESOURCE_RARE_MINERALS_ORE);
 
+	cc += resource(RESOURCE_COMMON_METALS);
+	cc += resource(RESOURCE_COMMON_MINERALS);
+	cc += resource(RESOURCE_RARE_METALS);
+	cc += resource(RESOURCE_RARE_MINERALS);
+
+	cc += resource(RESOURCE_FOOD);
+
+	return cc;
+}
+
+
+/**
+ * Returns the remaining capacity of the ResourcePool.
+ */
+int ResourcePool::remainingCapacity()
+{
+	int ret = capacity() - currentLevel();
+	return ret;
+}
+
+
+bool ResourcePool::atCapacity()
+{
+	return remainingCapacity() <= 0;
+}
+
+
+/**
+ * Attempt to push the defined amount of a specified resource into a ResourcePool.
+ * 
+ * \return Returns the remainder of the resource if any.
+ */
+int ResourcePool::pushResource(ResourceType type, int amount)
+{
+	if (_capacity == 0)
+	{
+		cout << "ResourcePool::pushResource(): Incorrect use of operator. This ResourcePool has no capacity. Use operator+=() instead." << endl;
+		return 0;
+	}
+
+	if (remainingCapacity() <= 0)
+	{
+		return amount;
+	}
+	else if (remainingCapacity() >= amount)
+	{
+		_resourceTable[type] += amount;
+		return 0;
+	}
+	else
+	{
+		int remainder = amount - remainingCapacity();
+		_resourceTable[type] += remainingCapacity();
+		return remainder;
+	}
+}
+
+/**
+ * Attempt to pull the defined amount of a specified resource.
+ * 
+ * \return Returns the amount of resources actually pulled.
+ */
+int ResourcePool::pullResource(ResourceType type, int amount)
+{
+	if (_capacity == 0)
+	{
+		cout << "ResourcePool::pullResource(): Incorrect use of operator. This ResourcePool has no capacity. Use operator-=() instead." << endl;
+		return 0;
+	}
+
+	return 0;
+}
+
+/**
+ * Attempts to push all available resources into a ResourcePool.
+ * 
+ * \param rp The ResourcePool to push resources from.
+ * 
+ * \return	Returns a reference to a ResourcePool with the remainder of
+ *			any resources that couldn't be pushed into the ResourcePool.
+ */
+void ResourcePool::pushResources(ResourcePool& rp)
+{
+	if (_capacity == 0)
+	{
+		cout << "ResourcePool::pushResources(): Incorrect use of operator. This ResourcePool has no capacity. Use operator+=() instead." << endl;
+		return;
+	}
+	
+	rp.commonMetalsOre(pushResource(RESOURCE_COMMON_METALS_ORE, rp.commonMetalsOre()));
+	rp.commonMineralsOre(pushResource(RESOURCE_COMMON_MINERALS_ORE, rp.commonMineralsOre()));
+	rp.rareMetalsOre(pushResource(RESOURCE_RARE_METALS_ORE, rp.rareMetalsOre()));
+	rp.rareMineralsOre(pushResource(RESOURCE_RARE_MINERALS_ORE, rp.rareMineralsOre()));
+
+	rp.commonMetals(pushResource(RESOURCE_COMMON_METALS, rp.commonMetals()));
+	rp.commonMinerals(pushResource(RESOURCE_COMMON_MINERALS, rp.commonMinerals()));
+	rp.rareMetals(pushResource(RESOURCE_RARE_METALS, rp.rareMetals()));
+	rp.rareMinerals(pushResource(RESOURCE_RARE_MINERALS, rp.rareMinerals()));
+
+	rp.food(pushResource(RESOURCE_FOOD, rp.food()));
+	rp.energy(pushResource(RESOURCE_ENERGY, rp.energy()));
+}
+
+/**
+ * Attempts to pull all available resources from a ResourcePool.
+ * 
+ * \param rp The ResourcePool to pull resources into.
+ */
+void ResourcePool::pullResources(ResourcePool& rp)
+{
+	if (_capacity == 0)
+	{
+		cout << "ResourcePool::pullResources(): Incorrect use of operator. This ResourcePool has no capacity. Use operator-=() instead." << endl;
+		return;
+	}
+}
+
+/**
+ * Sets the maximum amount of resources the ResourcePool can store.
+ */
+void ResourcePool::capacity(int _i)
+{
+	_capacity = _i;
+
+	// Prevent negative values.
+	if (_i < 0)
+		_capacity = 0;
+}
+
+
+// =======================================================================================================
+// = Comparison operators.
+// =======================================================================================================
 bool operator<(ResourcePool& lhs, ResourcePool& rhs)
 {
 	return (lhs.commonMetalsOre() < rhs.commonMetalsOre() &&
--- src/ResourcePool.h
+++ src/ResourcePool.h
@@ -47,39 +47,56 @@ public:
 	*/
 	void clear();
 
-	double resource(ResourceType _t);
-	void resource(ResourceType _t, double _d);
+	int resource(ResourceType _t);
+	void resource(ResourceType _t, int _i);
 
 	// GETTERS
-	double commonMetalsOre();
-	double commonMineralsOre();
-	double rareMetalsOre();
-	double rareMineralsOre();
+	int commonMetalsOre();
+	int commonMineralsOre();
+	int rareMetalsOre();
+	int rareMineralsOre();
 
-	double commonMetals();
-	double commonMinerals();
-	double rareMetals();
-	double rareMinerals();
+	int commonMetals();
+	int commonMinerals();
+	int rareMetals();
+	int rareMinerals();
 
-	double energy();
-	double food();
+	int energy();
+	int food();
 
 	// SETTERS
-	void commonMetalsOre(double _d);
-	void commonMineralsOre(double _d);
-	void rareMetalsOre(double _d);
-	void rareMineralsOre(double _d);
+	void commonMetalsOre(int _i);
+	void commonMineralsOre(int _i);
+	void rareMetalsOre(int _i);
+	void rareMineralsOre(int _i);
 
-	void commonMetals(double _d);
-	void commonMinerals(double _d);
-	void rareMetals(double _d);
-	void rareMinerals(double _d);
+	void commonMetals(int _i);
+	void commonMinerals(int _i);
+	void rareMetals(int _i);
+	void rareMinerals(int _i);
 
-	void energy(double _d);
-	void food(double _d);
+	void energy(int _i);
+	void food(int _i);
+
+
+	int pushResource(ResourceType type, int amount);
+	int pullResource(ResourceType type, int amount);
+
+	void pushResources(ResourcePool& rp);
+	void pullResources(ResourcePool& rp);
+
+	int capacity() const { return _capacity; }
+	void capacity(int _i);
+
+	int currentLevel();
+	int remainingCapacity();
+
+	bool atCapacity();
 
 private:
-	typedef std::map<ResourceType, double> ResourceTable;
+	typedef std::map<ResourceType, int> ResourceTable;
 
 	ResourceTable		_resourceTable;
+
+	int					_capacity;			// Maximum available capacity of the ResourcePool.
 };
