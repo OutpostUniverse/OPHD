2573ee26e29602d532b51a56b2516536d92cf636
Goof <Goof@bdf9137d-f3be-7946-88fe-7b0bbfdbf31a>
2016-08-07 21:54:59 +0000

    correct the type to STRUCTURE_RECREATION_CENTER instead of STRUCTURE_PARK
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outposthd@147 bdf9137d-f3be-7946-88fe-7b0bbfdbf31a

--- src/Things/Structures/RecreationCenter.h
+++ src/Things/Structures/RecreationCenter.h
@@ -7,7 +7,7 @@
 class RecreationCenter : public Structure
 {
 public:
-	RecreationCenter() : Structure(constants::RECREATION_CENTER, "structures/recreation_center.sprite", STRUCTURE_PARK)
+	RecreationCenter() : Structure(constants::RECREATION_CENTER, "structures/recreation_center.sprite", STRUCTURE_RECREATION_CENTER)
 	{
 		sprite().play(constants::STRUCTURE_STATE_CONSTRUCTION);
 		maxAge(500);
