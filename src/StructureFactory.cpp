5c5dedf6e460f9da1b3b1b8c8b138c428ccc3b56
leeor_net <leeor_net@bdf9137d-f3be-7946-88fe-7b0bbfdbf31a>
2017-07-25 20:13:17 +0000

    Updated StructureFactory to include population requirements table.
    - Changed from std::vector to std::array (tables don't ever need to be resized during runtime).
    - Updated documentation for correct usage of StructureFactory.
    
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outposthd@251 bdf9137d-f3be-7946-88fe-7b0bbfdbf31a

--- src/StructureFactory.cpp
+++ src/StructureFactory.cpp
@@ -1,7 +1,20 @@
 #include "StructureFactory.h"
 
-vector<ResourcePool> StructureFactory::mStructureCostTable;
 
+//vector<ResourcePool> StructureFactory::mStructureCostTable;
+std::array<ResourcePool, SID_COUNT> StructureFactory::mStructureCostTable;
+std::array<ResourcePool, SID_COUNT> StructureFactory::mStructureRecycleValueTable;
+std::array<PopulationRequirements, SID_COUNT> StructureFactory::mPopulationRequirementsTable;
+
+
+/**
+ * Gets a new Structure object given a StructureID.
+ * 
+ * \param	type	A valid StructureID value.
+ * 
+ * \return	Pointer to a newly constructed Structure or
+ *			\c nullptr if structure type unsupported/invalid.
+ */
 Structure* StructureFactory::get(StructureID type)
 {
 	Structure* _st = nullptr;
@@ -17,93 +30,123 @@ Structure* StructureFactory::get(StructureID type)
 		case SID_AGRIDOME:
 			_st = new Agridome();
 			break;
+
 		case SID_AIR_SHAFT:
 			_st = new AirShaft();
 			break;
+
 		case SID_CHAP:
 			_st = new CHAP();
 			break;
+
 		case SID_COLONIST_LANDER: // only here for loading games
 			_st = new ColonistLander(nullptr);
 			break;
+
 		case SID_COMMAND_CENTER:
 			_st = new CommandCenter();
 			break;
+
 		case SID_COMMERCIAL:
 			_st = new Commercial();
 			break;
+
 		case SID_FUSION_REACTOR:
 			_st = new FusionReactor();
 			break;
+
 		case SID_HOT_LABORATORY:
 			_st = new HotLaboratory();
 			break;
+
 		case SID_LABORATORY:
 			_st = new Laboratory();
 			break;
+
 		case SID_MEDICAL_CENTER:
 			_st = new MedicalCenter();
 			break;
+
 		case SID_MINE_FACILITY: // only here for loading games
 			_st = new MineFacility(nullptr);
 			break;
+
 		case SID_MINE_SHAFT: // only here for loading games
 			_st = new MineShaft();
 			break;
+
 		case SID_PARK:
 			_st = new Park();
 			break;
+
 		case SID_SURFACE_POLICE:
 			_st = new SurfacePolice();
 			break;
+
 		case SID_UNDERGROUND_POLICE:
 			_st = new UndergroundPolice();
 			break;
+
 		case SID_RECREATION_CENTER:
 			_st = new RecreationCenter();
 			break;
+
 		case SID_RED_LIGHT_DISTRICT:
 			_st = new RedLightDistrict();
 			break;
+
 		case SID_RESIDENCE:
 			_st = new Residence();
 			break;
+
 		case SID_ROBOT_COMMAND:
 			_st = new RobotCommand();
 			break;
+
 		case SID_SEED_FACTORY:
 			_st = new SeedFactory();
 			break;
+
 		case SID_SEED_LANDER: // only here for loading games
 			_st = new SeedLander(0, 0);
 			break;
+
 		case SID_SEED_POWER:
 			_st = new SeedPower();
 			break;
+
 		case SID_SEED_SMELTER:
 			_st = new SeedSmelter();
 			break;
+
 		case SID_SMELTER:
 			_st = new Smelter();
 			break;
+
 		case SID_SOLAR_PLANT:
 			_st = new SolarPlant();
 			break;
+
 		case SID_STORAGE_TANKS:
 			_st = new StorageTanks();
 			break;
+
 		case SID_SURFACE_FACTORY:
 			_st = new SurfaceFactory();
 			break;
+
 		case SID_UNDERGROUND_FACTORY:
 			_st = new UndergroundFactory();
 			break;
+
 		case SID_UNIVERSITY:
 			_st = new University();
 			break;
+
 		case SID_WAREHOUSE:
 			_st = new Warehouse();
 			break;
+
 		default:
 			cout << "StructureFactory::get(): Unsupported structure type called." << endl;
 			break;
@@ -113,6 +156,25 @@ Structure* StructureFactory::get(StructureID type)
 }
 
 
+/**
+ * Gets the population required to operate a given Structure.
+ * 
+ * \param	type	A valid StructureID value.
+ */
+const PopulationRequirements& StructureFactory::populationRequirements(StructureID type)
+{
+	if (mPopulationRequirementsTable.empty())
+		buildPopulationRequirementsTable();
+
+	return mPopulationRequirementsTable[type];
+}
+
+
+/**
+ * Gets the cost in resources required to build a given Structure.
+ * 
+ * \param	type	A valid StructureID value.
+ */
 const ResourcePool& StructureFactory::costToBuild(StructureID type)
 {
 	if (mStructureCostTable.empty())
@@ -122,10 +184,36 @@ const ResourcePool& StructureFactory::costToBuild(StructureID type)
 }
 
 
+/**
+ * Gets the recycling value of a specified structure type.
+ * 
+ * \param	type	A valid StructureID value.
+ */
+ResourcePool& StructureFactory::recyclingValue(StructureID type)
+{
+	if (mStructureCostTable.empty())
+		buildCostTable();
+
+	return mStructureCostTable[type];
+}
+
+
+/**
+ * Initializes StructureFactory and builds the requirements tables.
+ */
+void StructureFactory::init()
+{
+	buildCostTable();
+	buildRecycleValueTable();
+	buildPopulationRequirementsTable();
+}
+
+
+/**
+ * Fills out the build costs for all structures.
+ */
 void StructureFactory::buildCostTable()
 {
-	mStructureCostTable.resize(SID_COUNT);
-
 	mStructureCostTable[SID_AGRIDOME] = ResourcePool(0, 0, 0, 0, 20, 10, 5, 0, 0, 0);
 	mStructureCostTable[SID_AIR_SHAFT] = ResourcePool();
 	mStructureCostTable[SID_CHAP] = ResourcePool(0, 0, 0, 0, 50, 10, 20, 5, 0, 0);
@@ -155,23 +243,77 @@ void StructureFactory::buildCostTable()
 	mStructureCostTable[SID_UNDERGROUND_FACTORY] = ResourcePool(0, 0, 0, 0, 20, 10, 10, 5, 0, 0);
 	mStructureCostTable[SID_UNIVERSITY] = ResourcePool(0, 0, 0, 0, 20, 10, 10, 5, 0, 0);
 	mStructureCostTable[SID_WAREHOUSE] = ResourcePool(0, 0, 0, 0, 15, 5, 6, 1, 0, 0);
-
 }
 
 
-ResourcePool StructureFactory::recyclingValue(StructureID type)
+/**
+ * Fills out the recycle value for all structures.
+ */
+void StructureFactory::buildRecycleValueTable()
+{
+	for (size_t i = 0; i < SID_COUNT; ++i)
+		mStructureRecycleValueTable[static_cast<StructureID>(i)] = recycleValue(static_cast<StructureID>(i));
+}
+
+
+/**
+ * Calculates the base recycling value of a given structure.
+ * 
+ * \param	type	A valid StructureID value.
+ */
+ResourcePool StructureFactory::recycleValue(StructureID type)
 {
 	if (mStructureCostTable.empty())
-		buildCostTable();
+		throw std::runtime_error("StructureFactory::recycleValue() called before StructureFactory::buildCostTable().");
 
-	return ResourcePool(	mStructureCostTable[type].commonMetalsOre() * 0.9f,
-							mStructureCostTable[type].commonMineralsOre() * 0.9f,
-							mStructureCostTable[type].rareMetalsOre() * 0.9f,
-							mStructureCostTable[type].rareMineralsOre() * 0.9f,
-							mStructureCostTable[type].commonMetals() * 0.9f,
-							mStructureCostTable[type].commonMinerals() * 0.9f,
-							mStructureCostTable[type].rareMetals() * 0.9f,
-							mStructureCostTable[type].rareMinerals() * 0.9f,
-							mStructureCostTable[type].food() * 0.9f,
-							mStructureCostTable[type].energy() * 0.9f);
+	ResourcePool _rp = mStructureCostTable[type];
+
+	/** Truncation of value from float to int cast is intended and desired behavior. */
+	return ResourcePool(static_cast<int>(_rp.commonMetalsOre() * 0.9f),
+						static_cast<int>(_rp.commonMineralsOre() * 0.9f),
+						static_cast<int>(_rp.rareMetalsOre() * 0.9f),
+						static_cast<int>(_rp.rareMineralsOre() * 0.9f),
+						static_cast<int>(_rp.commonMetals() * 0.9f),
+						static_cast<int>(_rp.commonMinerals() * 0.9f),
+						static_cast<int>(_rp.rareMetals() * 0.9f),
+						static_cast<int>(_rp.rareMinerals() * 0.9f),
+						static_cast<int>(_rp.food() * 0.9f),
+						static_cast<int>(_rp.energy() * 0.9f));
+}
+
+
+/**
+ * Fills out the population requirements for all structures.
+ */
+void StructureFactory::buildPopulationRequirementsTable()
+{
+	mPopulationRequirementsTable[SID_AGRIDOME] = { 0 };
+	mPopulationRequirementsTable[SID_AIR_SHAFT] = { 0 };
+	mPopulationRequirementsTable[SID_CHAP] = { 0 };
+	mPopulationRequirementsTable[SID_COMMAND_CENTER] = { 0 };
+	mPopulationRequirementsTable[SID_COMMERCIAL] = { 0 };
+	mPopulationRequirementsTable[SID_FUSION_REACTOR] = { 0 };
+	mPopulationRequirementsTable[SID_HOT_LABORATORY] = { 0 };
+	mPopulationRequirementsTable[SID_LABORATORY] = { 0 };
+	mPopulationRequirementsTable[SID_MEDICAL_CENTER] = { 0 };
+	mPopulationRequirementsTable[SID_MINE_FACILITY] = { 0 };
+	mPopulationRequirementsTable[SID_PARK] = { 0 };
+	mPopulationRequirementsTable[SID_SURFACE_POLICE] = { 0 };
+	mPopulationRequirementsTable[SID_UNDERGROUND_POLICE] = { 0 };
+	mPopulationRequirementsTable[SID_RECREATION_CENTER] = { 0 };
+	mPopulationRequirementsTable[SID_RED_LIGHT_DISTRICT] = { 0 };
+	mPopulationRequirementsTable[SID_RESIDENCE] = { 0 };
+	mPopulationRequirementsTable[SID_ROBOT_COMMAND] = { 0 };
+	mPopulationRequirementsTable[SID_SEED_FACTORY] = { 0 };
+	mPopulationRequirementsTable[SID_SEED_LANDER] = { 0 };
+	mPopulationRequirementsTable[SID_SEED_POWER] = { 0 };
+	mPopulationRequirementsTable[SID_SEED_SMELTER] = { 0 };
+	mPopulationRequirementsTable[SID_SMELTER] = { 0 };
+	mPopulationRequirementsTable[SID_SOLAR_PLANT] = { 0 };
+	mPopulationRequirementsTable[SID_STORAGE_TANKS] = { 0 };
+	mPopulationRequirementsTable[SID_SURFACE_FACTORY] = { 0 };
+	mPopulationRequirementsTable[SID_TUBE] = { 0 };
+	mPopulationRequirementsTable[SID_UNDERGROUND_FACTORY] = { 0 };
+	mPopulationRequirementsTable[SID_UNIVERSITY] = { 0 };
+	mPopulationRequirementsTable[SID_WAREHOUSE] = { 0 };
 }
--- src/StructureFactory.h
+++ src/StructureFactory.h
@@ -5,25 +5,58 @@
 
 #include "Things/Structures/Structures.h"
 
+#include <array>
 
+/**
+ * Contains population requirements for a given Structure.
+ * 
+ * Index 0: Workers.
+ * Index 1: Scientists.
+ * 
+ * Any other index will result in exceptions being thrown.
+ */
+typedef std::array<int, 2> PopulationRequirements;
+
+
+/** 
+ * \class	StructureFactory
+ * \brief	Provides a means of instantiating new structures and getting build
+ *			cost / recycle value / population requirements.
+ * 
+ * StructureFactory is implemented as a static class and should never be
+ * instantiated.
+ * 
+ * \note	StructureFactory::init() must be called prior to use.
+ * 
+ * \code{.cpp}
+ * ResourcePool _rp = StructureFactory::costToBuild(SID_AGRIDOME);
+ * PopulationRequirements _pr = StructureFactory::populationRequirements(SID_AGRIDOME);
+ * \endcode
+ */
 class StructureFactory
 {
-public:
 
+public:
 	static Structure* get(StructureID type);
 
+	static const PopulationRequirements& populationRequirements(StructureID type);
 	static const ResourcePool& costToBuild(StructureID type);
-	static ResourcePool recyclingValue(StructureID type);
+	static ResourcePool& recyclingValue(StructureID type);
 
-protected:
+	static void init();
 
 private:
-	// Explicitly declared private to prevent instantiation.
-	StructureFactory() {}
-	~StructureFactory() {}
+	StructureFactory() {}	// Explicitly declared private to prevent instantiation.
+	~StructureFactory() {}	// Explicitly declared private to prevent instantiation.
 
 	static void buildCostTable();
+	static void buildPopulationRequirementsTable();
+	static void buildRecycleValueTable();
+	static ResourcePool recycleValue(StructureID type);
 
 private:
-	static vector<ResourcePool> mStructureCostTable;
+	//static vector<ResourcePool> mStructureCostTable;
+	static std::array<ResourcePool, SID_COUNT> mStructureCostTable;
+	static std::array<ResourcePool, SID_COUNT> mStructureRecycleValueTable;
+	static std::array<PopulationRequirements, SID_COUNT> mPopulationRequirementsTable;
 };
