450ca4190a9f20cac46300d7b4e368f4b4faad70
leeor_net <leeor_net@bdf9137d-f3be-7946-88fe-7b0bbfdbf31a>
2017-08-05 00:13:50 +0000

    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outposthd@317 bdf9137d-f3be-7946-88fe-7b0bbfdbf31a

---
+++ src/Things/Structures/Nursery.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Structure.h"
+
+#include "../../Constants.h"
+
+class Nursery : public Structure
+{
+public:
+	Nursery() : Structure(constants::NURSERY, "structures/nursery_01.sprite", CLASS_NURSERY)
+	{
+		sprite().play(constants::STRUCTURE_STATE_CONSTRUCTION);
+		maxAge(500);
+		turnsToBuild(4);
+
+		requiresCHAP(true);
+	}
+
+
+	virtual ~Nursery()
+	{}
+
+protected:
+
+	virtual void think()
+	{}
+
+	virtual void defineResourceInput()
+	{
+		resourcesIn().energy(1);
+	}
+
+	virtual void defineResourceOutput()
+	{}
+
+private:
+
+};
