ac01a3af97f3622e7cba8788b1916b39b962188a
leeor_net <leeor_net@bdf9137d-f3be-7946-88fe-7b0bbfdbf31a>
2018-03-25 13:46:16 +0000

    Adding required comment lines for PVS-Studio as per: https://www.viva64.com/en/b/0457/
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outposthd@386 bdf9137d-f3be-7946-88fe-7b0bbfdbf31a

--- src/Common.cpp
+++ src/Common.cpp
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include "Common.h"
 
 #include <NAS2D/NAS2D.h>
--- src/UI/Core/TextField.cpp
+++ src/UI/Core/TextField.cpp
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 // ==================================================================================
 // = NAS2D+GUI
 // = Copyright © 2008 - 2012, New Age Software
