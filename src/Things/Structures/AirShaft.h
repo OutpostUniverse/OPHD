c60c237cbf571904853e8452a7c6267d2a8aadcd
ldicker83 <leeor_net@yahoo.com>
2018-10-20 15:05:07 -0400

    Minor code formatting.

--- src/Things/Structures/AirShaft.h
+++ src/Things/Structures/AirShaft.h
@@ -2,10 +2,11 @@
 
 #include "Structure.h"
 
+
 class AirShaft: public Structure
 {
 public:
-	AirShaft():	Structure(constants::AIR_SHAFT, "structures/air_shaft.sprite", CLASS_TUBE), _ug(false)
+	AirShaft():	Structure(constants::AIR_SHAFT, "structures/air_shaft.sprite", CLASS_TUBE)
 	{
 		sprite().play(constants::STRUCTURE_STATE_OPERATIONAL);
 		maxAge(400);
@@ -16,20 +17,13 @@ public:
 	}
 
 
-	virtual ~AirShaft()
-	{
-	}
+	virtual ~AirShaft()	{}
 
 	void ug() { sprite().play(constants::STRUCTURE_STATE_OPERATIONAL_UG); }
 
-	virtual void forced_state_change(StructureState _s)
-	{
-		if (_ug)
-			return;
-	}
+	virtual void forced_state_change(StructureState _s) { if (_ug) { return; } }
 
 protected:
 private:
-
-	bool		_ug;
+	bool		_ug = false;
 };
