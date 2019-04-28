82cfea2a17dc031bb09befab3079ebf5c7f22e8a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2019-04-29 06:20:43 +0700

    Add Linux support for opening Wiki link

--- src/States/MainMenuState.cpp
+++ src/States/MainMenuState.cpp
@@ -245,6 +245,8 @@ void MainMenuState::btnHelpClicked()
 	system("start https://wiki.outpost2.net/doku.php?id=outposthd:how_to_play");
 #elif defined(__APPLE__)
 	system("open https://wiki.outpost2.net/doku.php?id=outposthd:how_to_play");
+#elif defined(__linux__)
+	system("xdg-open https://wiki.outpost2.net/doku.php?id=outposthd:how_to_play");
 #else
 	//#error Open a web page support on the current platform not implemented.
 	#pragma message( "Open a web page support on the current platform not implemented." )
