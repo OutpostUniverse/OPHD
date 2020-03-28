6ae653027dd252d25c89256d398b23e82175a402
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-03-28 04:13:27 -0600

    Use Point to hold text position, and convert type

--- src/States/MapViewStateDraw.cpp
+++ src/States/MapViewStateDraw.cpp
@@ -276,7 +276,8 @@ void MapViewState::drawNavInfo()
 		const auto levelString = (i == 0) ? std::string{"S"} : std::to_string(i); // Set string for current level
 		bool isCurrentDepth = i == mTileMap->currentDepth();
 		NAS2D::Color color = isCurrentDepth ? NAS2D::Color{255, 0, 0, 255} : NAS2D::Color{200, 200, 200, 255}; // red for current depth : white for others
-		renderer.drawText(*MAIN_FONT, levelString, iPosX - MAIN_FONT->width(levelString), iPosY, color.red(), color.green(), color.blue(), color.alpha());
+		const auto position = NAS2D::Point<int>{iPosX - MAIN_FONT->width(levelString), iPosY}.to<float>();
+		renderer.drawText(*MAIN_FONT, levelString, position.x(), position.y(), color.red(), color.green(), color.blue(), color.alpha());
 		iPosX = iPosX - iWidth; // Shift position by one step left
 	}
 }
