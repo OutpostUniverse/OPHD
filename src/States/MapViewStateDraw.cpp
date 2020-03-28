2d29ee328a22396210955bff95e435409640c7f5
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-03-28 12:14:50 -0600

    Use vectorized drawText in drawNavInfo()

--- src/States/MapViewStateDraw.cpp
+++ src/States/MapViewStateDraw.cpp
@@ -279,7 +279,7 @@ void MapViewState::drawNavInfo()
 		bool isCurrentDepth = i == mTileMap->currentDepth();
 		NAS2D::Color color = isCurrentDepth ? NAS2D::Color{255, 0, 0, 255} : NAS2D::Color{200, 200, 200, 255}; // red for current depth : white for others
 		const auto position = NAS2D::Point<int>{iPosX - MAIN_FONT->width(levelString), iPosY}.to<float>();
-		renderer.drawText(*MAIN_FONT, levelString, position.x(), position.y(), color.red(), color.green(), color.blue(), color.alpha());
+		renderer.drawText(*MAIN_FONT, levelString, position, color);
 		iPosX = iPosX - iWidth; // Shift position by one step left
 	}
 }
