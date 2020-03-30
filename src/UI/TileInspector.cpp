058ad9af24b3cb99daf4db1e870b995067c92d63
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-03-30 04:16:25 -0600

    Create lambda to print TileInspector title and text line
    Test it out on the first line of text.

--- src/UI/TileInspector.cpp
+++ src/UI/TileInspector.cpp
@@ -53,12 +53,19 @@ void TileInspector::update()
 
 	Renderer& r = Utility<Renderer>::get();
 
-	r.drawText(*FONT_BOLD, "Has Mine:", rect().x() + 5, rect().y() + 25, 255, 255, 255);
+	const auto drawTitleText = [&renderer = r](NAS2D::Point<int> position, std::string title, std::string text) {
+		renderer.drawText(*FONT_BOLD, title, position, NAS2D::Color::White);
+		position.x() += FONT_BOLD->width(title);
+		renderer.drawText(*FONT, text, position, NAS2D::Color::White);
+	};
+
+	const auto* mine = mTile->mine();
+
+	auto position = rect().startPoint() + NAS2D::Vector{5, 25};
+	drawTitleText(position, "Has Mine: ", (mine ? "Yes" : "No"));
 
 	if(mTile->mine())
 	{
-		r.drawText(*FONT, "Yes", rect().x() + 5 + FONT_BOLD->width("Has Mine: "), rect().y() + 25, 255, 255, 255);
-
 		r.drawText(*FONT_BOLD, "Active:", rect().x() + 5, rect().y() + 35, 255, 255, 255);
 
 		if (mTile->mine()->active())
@@ -72,10 +79,6 @@ void TileInspector::update()
 
 		r.drawText(*FONT_BOLD, "Production Rate:", rect().x() + 5, rect().y() + 45, 255, 255, 255);
 		r.drawText(*FONT, MINE_YIELD_TRANSLATION[mTile->mine()->productionRate()], rect().x() + 5 + FONT_BOLD->width("Production Rate: "), rect().y() + 45, 255, 255, 255);
-	}
-	else
-	{
-		r.drawText(*FONT, "No", rect().x() + 5 + FONT_BOLD->width("Has Mine: "), rect().y() + 25, 255, 255, 255);
 	}
 
 	r.drawText(*FONT_BOLD, "Location:", rect().x() + 5, rect().y() + 62, 255, 255, 255);
