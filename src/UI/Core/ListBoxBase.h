bd6a7f5e6c72aeaf375cd2fc60fd65b5e40d4eab
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-03-31 08:43:38 -0600

    Fix Doxygen command

--- src/UI/Core/ListBoxBase.h
+++ src/UI/Core/ListBoxBase.h
@@ -19,7 +19,7 @@
  * input handling and management code while leaving the specific implementation
  * to more derived types.
  * 
- * \notes	This is an abstract class -- it will need to be inherited from
+ * \note	This is an abstract class -- it will need to be inherited from
  *			in order to be used.
  */
 class ListBoxBase : public UIContainer
