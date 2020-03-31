331d9914d12a2654d2eb6a1202bc21d58376d648
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-03-31 07:47:49 -0600

    Mark position as const

--- src/Map/TileMap.cpp
+++ src/Map/TileMap.cpp
@@ -341,7 +341,7 @@ void TileMap::draw()
 
 			if (tile.excavated())
 			{
-				auto position = mMapPosition + NAS2D::Vector{(col - row) * TILE_HALF_WIDTH, (col + row) * TILE_HEIGHT_HALF_ABSOLUTE};
+				const auto position = mMapPosition + NAS2D::Vector{(col - row) * TILE_HALF_WIDTH, (col + row) * TILE_HEIGHT_HALF_ABSOLUTE};
 				const auto subImageRect = NAS2D::Rectangle{tile.index() * TILE_WIDTH, tsetOffset, TILE_WIDTH, TILE_HEIGHT};
 				const bool isTileHighlighted = row == mMapHighlight.y() && col == mMapHighlight.x();
 				const bool isConnectionHighlighted = mShowConnections && tile.connected();
