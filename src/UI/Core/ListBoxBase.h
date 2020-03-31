7d49c05be3c28daa9efb8041bcd390c8884a700f
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-03-31 08:43:09 -0600

    Correct comment about abstract class

--- src/UI/Core/ListBoxBase.h
+++ src/UI/Core/ListBoxBase.h
@@ -19,7 +19,7 @@
  * input handling and management code while leaving the specific implementation
  * to more derived types.
  * 
- * \notes	This is a pure virtual function -- it will need to be inherited from
+ * \notes	This is an abstract class -- it will need to be inherited from
  *			in order to be used.
  */
 class ListBoxBase : public UIContainer
