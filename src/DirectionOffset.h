32d1ec8d3fc2169669c943cb7bb69425152a39b6
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-04-03 11:19:59 -0600

    Simplify direction constants using deduction guides

--- src/DirectionOffset.h
+++ src/DirectionOffset.h
@@ -5,15 +5,15 @@
 #include <array>
 
 
-constexpr auto DirectionNorthWest = NAS2D::Vector<int>{-1, -1};
-constexpr auto DirectionNorth     = NAS2D::Vector<int>{ 0, -1};
-constexpr auto DirectionNorthEast = NAS2D::Vector<int>{ 1, -1};
-constexpr auto DirectionWest      = NAS2D::Vector<int>{-1,  0};
-constexpr auto DirectionCenter    = NAS2D::Vector<int>{ 0,  0};
-constexpr auto DirectionEast      = NAS2D::Vector<int>{ 1,  0};
-constexpr auto DirectionSouthWest = NAS2D::Vector<int>{-1,  1};
-constexpr auto DirectionSouth     = NAS2D::Vector<int>{ 0,  1};
-constexpr auto DirectionSouthEast = NAS2D::Vector<int>{ 1,  1};
+constexpr auto DirectionNorthWest = NAS2D::Vector{-1, -1};
+constexpr auto DirectionNorth     = NAS2D::Vector{ 0, -1};
+constexpr auto DirectionNorthEast = NAS2D::Vector{ 1, -1};
+constexpr auto DirectionWest      = NAS2D::Vector{-1,  0};
+constexpr auto DirectionCenter    = NAS2D::Vector{ 0,  0};
+constexpr auto DirectionEast      = NAS2D::Vector{ 1,  0};
+constexpr auto DirectionSouthWest = NAS2D::Vector{-1,  1};
+constexpr auto DirectionSouth     = NAS2D::Vector{ 0,  1};
+constexpr auto DirectionSouthEast = NAS2D::Vector{ 1,  1};
 
 
 constexpr auto DirectionScan3x3 = std::array<NAS2D::Vector<int>, 9>{
