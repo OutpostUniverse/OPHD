8630b78b787806f8a06ba736ff7609f70fbbb980
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-04-20 02:20:33 -0600

    Pull out long Font expression into const temporary

--- OPHD/UI/MajorEventAnnouncement.cpp
+++ OPHD/UI/MajorEventAnnouncement.cpp
@@ -71,6 +71,6 @@ void MajorEventAnnouncement::update()
 	r.drawImage(mHeader, rect().x() + 5, rect().y() + 25);
 
 	// Yeah, I know. I hate it too but it made more sense than holding onto a static pointer.
-	r.drawText(	*Utility<FontManager>::get().font(constants::FONT_PRIMARY, constants::FONT_PRIMARY_NORMAL),
-				mMessage, rect().x() + 5, rect().y() + 290, 255, 255, 255);
+	const auto& font = *Utility<FontManager>::get().font(constants::FONT_PRIMARY, constants::FONT_PRIMARY_NORMAL);
+	r.drawText(font, mMessage, rect().x() + 5, rect().y() + 290, 255, 255, 255);
 }
