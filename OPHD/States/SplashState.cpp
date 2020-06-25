3f67d9c7b5d17a898617ab0c50f9548c6508ec94
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-06-24 23:07:01 -0600

    Use packed parameter call to set SplashState backgroundColor

--- OPHD/States/SplashState.cpp
+++ OPHD/States/SplashState.cpp
@@ -109,8 +109,8 @@ NAS2D::State* SplashState::update()
 	}
 
 	const auto size = renderer.size();
-	if (CURRENT_STATE == LogoState::LOGO_OUTPOSTHD) { renderer.drawBoxFilled(0, 0, size.x, size.y, 0, 0, 0); }
-	else { renderer.drawBoxFilled(0, 0, size.x, size.y, 255, 255, 255); }
+	const auto backgroundColor = (CURRENT_STATE == LogoState::LOGO_OUTPOSTHD) ? NAS2D::Color::Black : NAS2D::Color::White;
+	renderer.drawBoxFilled({0, 0, size.x, size.y}, backgroundColor);
 
 
 	if (CURRENT_STATE == LogoState::LOGO_LAIRWORKS)
