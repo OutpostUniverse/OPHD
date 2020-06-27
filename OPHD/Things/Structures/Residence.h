4b61a3fcf24ae41537a4974f10a84f65ad5e165f
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-06-27 18:07:23 -0400

    Add Residence capacity to structure inspector window

--- OPHD/Things/Structures/Residence.h
+++ OPHD/Things/Structures/Residence.h
@@ -2,7 +2,10 @@
 
 #include "Structure.h"
 
+#include "../../FontManager.h"
 #include "../../Constants.h"
+#include <NAS2D/Resources/Font.h>
+#include <NAS2D/Utility.h>
 
 
 /**
@@ -25,6 +28,21 @@ public:
 	}
 
 	int capacity() const { return mCapacity; }
+
+	void drawInspectorView(NAS2D::Renderer& renderer, const NAS2D::Rectangle<float>& windowRect) override
+	{
+		const auto drawLabelAndValue = [&renderer](NAS2D::Point<int> position, const std::string& title, const std::string& text) {
+			NAS2D::Font* FONT = NAS2D::Utility<FontManager>::get().font(constants::FONT_PRIMARY, constants::FONT_PRIMARY_NORMAL);
+			NAS2D::Font* FONT_BOLD = NAS2D::Utility<FontManager>::get().font(constants::FONT_PRIMARY_BOLD, constants::FONT_PRIMARY_NORMAL);
+
+			renderer.drawText(*FONT_BOLD, title, position, NAS2D::Color::White);
+			position.x() += FONT_BOLD->width(title);
+			renderer.drawText(*FONT, text, position, NAS2D::Color::White);
+		};
+
+		auto position = windowRect.startPoint() + NAS2D::Vector{ 10, 135 };
+		drawLabelAndValue(position, "Colonist Capacity: ", std::to_string(capacity()));
+	}
 
 protected:
 	void defineResourceInput() override
