2d3ef5d9e2c738022311d2f85937f76c380548c8
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-06-27 22:50:57 -0400

    Add color to drawLabelAndValue

--- OPHD/UI/TextRender.cpp
+++ OPHD/UI/TextRender.cpp
@@ -8,16 +8,16 @@
 #include <NAS2D/Utility.h>
 
 
-void drawLabelAndValue(NAS2D::Point<int> position, const std::string& title, const std::string& text) 
+void drawLabelAndValue(NAS2D::Point<int> position, const std::string& title, const std::string& text, NAS2D::Color color) 
 {
 	auto& renderer = NAS2D::Utility<NAS2D::Renderer>::get();
 
 	NAS2D::Font* FONT = NAS2D::Utility<FontManager>::get().font(constants::FONT_PRIMARY, constants::FONT_PRIMARY_NORMAL);
 	NAS2D::Font* FONT_BOLD = NAS2D::Utility<FontManager>::get().font(constants::FONT_PRIMARY_BOLD, constants::FONT_PRIMARY_NORMAL);
 
-	renderer.drawText(*FONT_BOLD, title, position, NAS2D::Color::White);
+	renderer.drawText(*FONT_BOLD, title, position, color);
 	position.x() += FONT_BOLD->width(title);
-	renderer.drawText(*FONT, text, position, NAS2D::Color::White);
+	renderer.drawText(*FONT, text, position, color);
 }
 
 void drawLabelAndValueLeftJustify(NAS2D::Point<int> position, const std::string& title, const std::string& text, int labelWidth, NAS2D::Color color) 
