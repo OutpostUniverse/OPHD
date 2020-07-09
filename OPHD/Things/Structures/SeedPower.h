f35ece2c1e0470212639117e4285450f59661a90
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-07-08 23:17:29 -0400

    Add power produced to Seed Power structure inspector window

--- OPHD/Things/Structures/SeedPower.h
+++ OPHD/Things/Structures/SeedPower.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Structure.h"
+#include <string>
 
 const int SEED_POWER_PRODUCTION = 50;
 
@@ -16,9 +17,33 @@ public:
 		requiresCHAP(false);
 	}
 
+	StringTable createInspectorViewTable() override
+	{
+		StringTable stringTable(2, 1);
+
+		stringTable[{0, 0}].text = "Power Produced:";
+
+		auto energyProduced = calculateEnergyProduced();
+
+		stringTable[{1, 0}].text = std::to_string(energyProduced) + " / " + std::to_string(SEED_POWER_PRODUCTION);
+
+		if (energyProduced == 0)
+		{
+			stringTable[{1, 0}].textColor = constants::WARNING_TEXT_COLOR;
+		}
+
+		return stringTable;
+	}
+
 protected:
 	void defineResourceOutput() override
 	{
-		resourcesOut().energy(SEED_POWER_PRODUCTION);
+		resourcesOut().energy(calculateEnergyProduced());
+	}
+
+private:
+	int calculateEnergyProduced()
+	{
+		return operational() ? SEED_POWER_PRODUCTION : 0;
 	}
 };
