80dc9a67380ee58bdb6ba8b14217caff7d3d126e
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-07-08 20:52:32 -0400

    Add power produced to fusion power plant structure view

--- OPHD/Things/Structures/FusionReactor.h
+++ OPHD/Things/Structures/FusionReactor.h
@@ -3,6 +3,7 @@
 #include "Structure.h"
 
 #include "../../Constants.h"
+#include <string>
 
 const int FUSION_REACTOR_BASE_PRODUCUCTION = 1000;
 
@@ -17,6 +18,24 @@ public:
 		requiresCHAP(false);
 	}
 
+	StringTable createInspectorViewTable() override
+	{
+		StringTable stringTable(2, 1);
+
+		stringTable[{0, 0}].text = "Power Produced:";
+
+		auto energyProduced = calculateEnergyProduced();
+
+		stringTable[{1, 0}].text = std::to_string(energyProduced) + " / " + std::to_string(FUSION_REACTOR_BASE_PRODUCUCTION);
+
+		if (energyProduced == 0)
+		{
+			stringTable[{1, 0}].textColor = constants::WARNING_TEXT_COLOR;
+		}
+
+		return stringTable;
+	}
+
 protected:
 	void defineResourceInput() override
 	{
@@ -28,6 +47,12 @@ protected:
 
 	void defineResourceOutput() override
 	{
-		resourcesOut().energy(FUSION_REACTOR_BASE_PRODUCUCTION);
+		resourcesOut().energy(calculateEnergyProduced());
+	}
+
+private:
+	int calculateEnergyProduced()
+	{
+		return operational() ? FUSION_REACTOR_BASE_PRODUCUCTION : 0;
 	}
 };
