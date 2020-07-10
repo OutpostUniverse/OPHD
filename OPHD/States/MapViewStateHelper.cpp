4518db1540a877ecbb6d5436ac434e73d0ff45f2
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-07-09 23:51:11 -0600

    Factor out common StructureManager expression

--- OPHD/States/MapViewStateHelper.cpp
+++ OPHD/States/MapViewStateHelper.cpp
@@ -320,14 +320,15 @@ bool outOfCommRange(Point<int>& cc_location, TileMap* tile_map, Tile* current_ti
 	if (tile->distanceTo(tile_map->getTile(cc_location, 0)) <= constants::ROBOT_COM_RANGE)
 		return false;
 
-	for (auto _tower : Utility<StructureManager>::get().structureList(Structure::StructureClass::CLASS_COMM))
+	auto structureManager = Utility<StructureManager>::get();
+	for (auto _tower : structureManager.structureList(Structure::StructureClass::CLASS_COMM))
 	{
 		if (!_tower->operational())
 		{
 			continue;
 		}
 
-		const auto _comm_t = Utility<StructureManager>::get().tileFromStructure(_tower);
+		const auto _comm_t = structureManager.tileFromStructure(_tower);
 		if (_comm_t->distanceTo(current_tile) <= constants::COMM_TOWER_BASE_RANGE)
 		{
 			return false;
