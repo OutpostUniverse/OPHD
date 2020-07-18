01dcc77349284efff79c1cb4eb728f5d3ebc9604
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-07-18 06:20:57 -0600

    Rename variable for stepSizeWidth

--- OPHD/States/MapViewStateDraw.cpp
+++ OPHD/States/MapViewStateDraw.cpp
@@ -278,7 +278,7 @@ void MapViewState::drawNavInfo()
 	drawNavIcon(renderer, MOVE_SOUTH_ICON, NAS2D::Rectangle{0, 144, 32, 16}, NAS2D::Color::White, NAS2D::Color::Red);
 
 	// display the levels "bar"
-	int iWidth = MAIN_FONT->width("IX"); // set steps character patern width
+	const auto stepSizeWidth = MAIN_FONT->width("IX");
 	auto position = NAS2D::Point{static_cast<int>(renderer.width()) - 5, mMiniMapBoundingBox.y - 30};
 
 	for (int i = mTileMap->maxDepth(); i >= 0; i--)
@@ -288,6 +288,6 @@ void MapViewState::drawNavInfo()
 		bool isCurrentDepth = i == mTileMap->currentDepth();
 		NAS2D::Color color = isCurrentDepth ? NAS2D::Color::Red : NAS2D::Color{200, 200, 200}; // red for current depth : white for others
 		renderer.drawText(*MAIN_FONT, levelString, position - textSize, color);
-		position.x -= iWidth; // Shift position by one step left
+		position.x -= stepSizeWidth;
 	}
 }
