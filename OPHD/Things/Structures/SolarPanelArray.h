db1bafa3adfc1c17a7fb780aeb66f36d23e22672
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-07-18 15:45:51 -0400

    Display solar panel power produced

--- OPHD/Things/Structures/SolarPanelArray.h
+++ OPHD/Things/Structures/SolarPanelArray.h
@@ -17,9 +17,33 @@ public:
 		requiresCHAP(false);
 	}
 
+	StringTable createInspectorViewTable() override
+	{
+		StringTable stringTable(2, 1);
+
+		stringTable[{0, 0}].text = "Power Produced:";
+
+		auto energyProduced = calculateEnergyProduced();
+
+		stringTable[{1, 0}].text = std::to_string(energyProduced) + " / " + std::to_string(SOLAR_PANEL_BASE_PRODUCUCTION);
+
+		if (energyProduced == 0)
+		{
+			stringTable[{1, 0}].textColor = constants::WARNING_TEXT_COLOR;
+		}
+
+		return stringTable;
+	}
+
 protected:
 	void defineResourceOutput() override
 	{
-		resourcesOut().energy(SOLAR_PANEL_BASE_PRODUCUCTION);
+		resourcesOut().energy(calculateEnergyProduced());
+	}
+
+private:
+	int calculateEnergyProduced()
+	{
+		return operational() ? SOLAR_PANEL_BASE_PRODUCUCTION : 0;
 	}
 };
