550ac97cdd31ee21cbf55097bad8da391fa6e7eb
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-07-19 14:04:30 -0400

    Use PowerStructure as base class for FusionReactor, SeedPower, and SolarPanelArray

--- OPHD/Things/Structures/SeedPower.h
+++ OPHD/Things/Structures/SeedPower.h
@@ -1,38 +1,20 @@
 #pragma once
 
 #include "Structure.h"
+#include "../../Constants.h"
 #include <string>
 
 const int SEED_POWER_PRODUCTION = 50;
 
-class SeedPower: public Structure
+class SeedPower: public PowerStructure
 {
 public:
-	SeedPower() :
-		Structure(constants::SEED_POWER, "structures/seed_1.sprite", StructureClass::CLASS_ENERGY_PRODUCTION)
+	SeedPower() : PowerStructure(constants::SEED_POWER, "structures/seed_1.sprite", StructureClass::CLASS_ENERGY_PRODUCTION)
 	{
 		sprite().play(constants::STRUCTURE_STATE_CONSTRUCTION);
 		maxAge(150);
 		turnsToBuild(5);
 		requiresCHAP(false);
-	}
-
-	StringTable createInspectorViewTable() override
-	{
-		StringTable stringTable(2, 1);
-
-		stringTable[{0, 0}].text = "Power Produced:";
-
-		auto energyProduced = calculateEnergyProduced();
-
-		stringTable[{1, 0}].text = std::to_string(energyProduced) + " / " + std::to_string(SEED_POWER_PRODUCTION);
-
-		if (energyProduced == 0)
-		{
-			stringTable[{1, 0}].textColor = constants::WARNING_TEXT_COLOR;
-		}
-
-		return stringTable;
 	}
 
 protected:
@@ -42,8 +24,13 @@ protected:
 	}
 
 private:
-	int calculateEnergyProduced()
+	int calculateEnergyProduced() override
 	{
 		return operational() ? SEED_POWER_PRODUCTION : 0;
+	}
+
+	int calculateMaxEnergyProduction() override
+	{
+		return SEED_POWER_PRODUCTION;
 	}
 };
--- OPHD/Things/Structures/SolarPanelArray.h
+++ OPHD/Things/Structures/SolarPanelArray.h
@@ -1,38 +1,20 @@
 #pragma once
 
-#include "Structure.h"
+#include "PowerStructure.h"
 
 #include "../../Constants.h"
 
 const int SOLAR_PANEL_BASE_PRODUCUCTION = 25;
 
-class SolarPanelArray : public Structure
+class SolarPanelArray : public PowerStructure
 {
 public:
-	SolarPanelArray() : Structure(constants::SOLAR_PANEL1, "structures/solar_array1.sprite", StructureClass::CLASS_ENERGY_PRODUCTION)
+	SolarPanelArray() : PowerStructure(constants::SOLAR_PANEL1, "structures/solar_array1.sprite", StructureClass::CLASS_ENERGY_PRODUCTION)
 	{
 		sprite().play(constants::STRUCTURE_STATE_CONSTRUCTION);
 		maxAge(1000);
 		turnsToBuild(4);
 		requiresCHAP(false);
-	}
-
-	StringTable createInspectorViewTable() override
-	{
-		StringTable stringTable(2, 1);
-
-		stringTable[{0, 0}].text = "Power Produced:";
-
-		auto energyProduced = calculateEnergyProduced();
-
-		stringTable[{1, 0}].text = std::to_string(energyProduced) + " / " + std::to_string(SOLAR_PANEL_BASE_PRODUCUCTION);
-
-		if (energyProduced == 0)
-		{
-			stringTable[{1, 0}].textColor = constants::WARNING_TEXT_COLOR;
-		}
-
-		return stringTable;
 	}
 
 protected:
@@ -42,8 +24,13 @@ protected:
 	}
 
 private:
-	int calculateEnergyProduced()
+	int calculateEnergyProduced() override
 	{
 		return operational() ? SOLAR_PANEL_BASE_PRODUCUCTION : 0;
+	}
+
+	int calculateMaxEnergyProduction() override
+	{
+		return SOLAR_PANEL_BASE_PRODUCUCTION;
 	}
 };
