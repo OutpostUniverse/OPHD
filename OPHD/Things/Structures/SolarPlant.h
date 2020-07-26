f943dd15c62163d157491d070366dcd70da3f926
Brett208 <15183337+Brett208@users.noreply.github.com>
2020-07-26 12:40:20 -0400

    Prevent possible divide by zero in SolarPlant & SolarPanelArray
    If zero is present, use 1, which would present reasonable results for gameplay.

--- OPHD/Things/Structures/SolarPlant.h
+++ OPHD/Things/Structures/SolarPlant.h
@@ -26,7 +26,10 @@ protected:
 
 	int calculateMaxEnergyProduction() override
 	{
-		return static_cast<int>(SOLAR_PLANT_BASE_PRODUCUCTION / mMeanSolarDistance);
+		// Prevent possible dividing by zero
+		float solarDistance = mMeanSolarDistance != 0 ? mMeanSolarDistance : 1;
+
+		return static_cast<int>(SOLAR_PLANT_BASE_PRODUCUCTION / solarDistance);
 	}
 
 private:
