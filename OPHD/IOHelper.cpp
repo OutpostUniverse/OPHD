2d19b38ed6b276351c88836680784a48aaa32c44
ldicker83 <leeor_net@yahoo.com>
2020-08-04 22:39:55 -0400

    Add readResources to IOHelper

--- OPHD/IOHelper.cpp
+++ OPHD/IOHelper.cpp
@@ -4,9 +4,24 @@
 
 #include "StorableResources.h"
 
-/**
- * 
- */
+
+void readResources(NAS2D::Xml::XmlElement* element, StorableResources& resources)
+{
+	if (!element) { throw std::runtime_error("MapViewState::readResources(): Called with element==nullptr"); }
+
+	NAS2D::Xml::XmlAttribute* attribute = element->firstAttribute();
+	while (attribute)
+	{
+		if (attribute->name() == constants::SAVE_GAME_RESOURCE_0) { attribute->queryIntValue(resources.resources[0]); }
+		else if (attribute->name() == constants::SAVE_GAME_RESOURCE_1) { attribute->queryIntValue(resources.resources[1]); }
+		else if (attribute->name() == constants::SAVE_GAME_RESOURCE_2) { attribute->queryIntValue(resources.resources[2]); }
+		else if (attribute->name() == constants::SAVE_GAME_RESOURCE_3) { attribute->queryIntValue(resources.resources[3]); }
+
+		attribute = attribute->next();
+	}
+}
+
+
 void writeResources(NAS2D::Xml::XmlElement* element, const StorableResources& resources, const std::string& tagName)
 {
 	NAS2D::Xml::XmlElement* resources_elem = new NAS2D::Xml::XmlElement(tagName);
--- OPHD/IOHelper.h
+++ OPHD/IOHelper.h
@@ -3,6 +3,8 @@
 #include <NAS2D/Xml/Xml.h>
 #include <string>
 
-class StorableResources;
+struct StorableResources;
+
+void readResources(NAS2D::Xml::XmlElement* element, StorableResources& resources);
 
 void writeResources(NAS2D::Xml::XmlElement*, const StorableResources&, const std::string&);
