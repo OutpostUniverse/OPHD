b1c970990818803d574040dc75f1b82499ead916
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-08-18 10:51:58 -0600

    Reorder header includes

--- OPHD/UI/ResourceBreakdownPanel.cpp
+++ OPHD/UI/ResourceBreakdownPanel.cpp
@@ -7,9 +7,9 @@
 #include "../Common.h"
 #include "../Constants.h"
 
+#include <NAS2D/StringUtils.h>
 #include <NAS2D/Utility.h>
 #include <NAS2D/Renderer/Renderer.h>
-#include <NAS2D/StringUtils.h>
 
 #include <array>
 
