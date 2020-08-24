5e5d2c0431e2172ef33b35088cdcbc6de960fe3c
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-08-24 01:01:42 -0600

    Move public struct fields first and helper methods last

--- OPHD/UI/Core/ListBox.h
+++ OPHD/UI/Core/ListBox.h
@@ -28,11 +28,11 @@ public:
 
 	struct ListBoxItem
 	{
-		bool operator==(const std::string& rhs) { return Text == rhs; }
-		bool operator<(const ListBoxItem& lhs) { return Text < lhs.Text; }
-
 		std::string Text; /**< Text of the ListBoxItem. */
 		int Tag = 0; /**< User defined int data attached to the item. */
+
+		bool operator==(const std::string& rhs) { return Text == rhs; }
+		bool operator<(const ListBoxItem& lhs) { return Text < lhs.Text; }
 	};
 
 	using ListBoxItems = std::vector<ListBoxItem>;
