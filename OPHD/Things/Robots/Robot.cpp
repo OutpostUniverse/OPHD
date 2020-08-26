a8e09c4317b3d58e39e34b67e72adc3d6aff1c58
malry <58125586+malryt@users.noreply.github.com>
2020-08-26 14:33:10 -0400

    Remove PVS-Studio comments

--- OPHD/Things/Robots/Robot.cpp
+++ OPHD/Things/Robots/Robot.cpp
@@ -1,6 +1,3 @@
-// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
 #include "Robot.h"
 
 Robot::Robot(const std::string& name, const std::string& sprite_path) :
