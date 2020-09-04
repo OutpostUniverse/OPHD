2b1018f330f7768583a0f90f5c4288d40fc29ea0
ldicker83 <leeor_net@yahoo.com>
2020-09-03 21:11:30 -0400

    Update calls to renamed MapViewState::refinedResourcesInStorage()

--- OPHD/States/MapViewStateDraw.cpp
+++ OPHD/States/MapViewStateDraw.cpp
@@ -174,7 +174,7 @@ void MapViewState::drawResourceInfo()
 	// Capacity (Storage, Food, Energy)
 	const std::array storageCapacities
 	{
-		std::tuple{NAS2D::Rectangle{96, 32, iconSize, iconSize}, resourcesInStorage(), totalStorage(Structure::StructureClass::Storage, 1000), totalStorage(Structure::StructureClass::Storage, 1000) - resourcesInStorage() <= 100},
+		std::tuple{NAS2D::Rectangle{96, 32, iconSize, iconSize}, refinedResourcesInStorage(), totalStorage(Structure::StructureClass::Storage, 1000), totalStorage(Structure::StructureClass::Storage, 1000) - refinedResourcesInStorage() <= 100},
 		std::tuple{NAS2D::Rectangle{64, 32, iconSize, iconSize}, mFood, totalStorage(Structure::StructureClass::FoodProduction, 1000), mFood <= 10},
 		std::tuple{NAS2D::Rectangle{80, 32, iconSize, iconSize}, mEnergy, NAS2D::Utility<StructureManager>::get().totalEnergyProduction(), mEnergy <= 5}
 	};
