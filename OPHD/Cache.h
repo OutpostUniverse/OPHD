d01ddc90ca90a83ee4b41f9f11b6219c4cb47154
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-09-11 00:55:33 -0600

    Update NAS2D submodule for ResourceCache namespace fix

--- OPHD/Cache.h
+++ OPHD/Cache.h
@@ -8,7 +8,7 @@
 #include <memory>
 
 
-inline ResourceCache<NAS2D::Font, std::string, unsigned int> fontCache;
-inline ResourceCache<NAS2D::Image, std::string> imageCache;
+inline NAS2D::ResourceCache<NAS2D::Font, std::string, unsigned int> fontCache;
+inline NAS2D::ResourceCache<NAS2D::Image, std::string> imageCache;
 
 inline std::unique_ptr<NAS2D::Music> trackMars;
