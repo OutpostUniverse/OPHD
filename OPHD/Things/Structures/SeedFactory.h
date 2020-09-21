243b4b65387474079fc3939136ace4be28a85616
ldicker83 <leeor_net@yahoo.com>
2020-09-21 16:04:28 -0400

    Add truck manufacturing to SeedFactory and SurfaceFactory

--- OPHD/Things/Structures/SeedFactory.h
+++ OPHD/Things/Structures/SeedFactory.h
@@ -24,6 +24,7 @@ protected:
 		addProduct(ProductType::PRODUCT_DIGGER);
 		addProduct(ProductType::PRODUCT_DOZER);
 		addProduct(ProductType::PRODUCT_MINER);
+		addProduct(ProductType::PRODUCT_TRUCK);
 	}
 
 private:
