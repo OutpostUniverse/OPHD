623f48af209cd600310101986f5f181b57a754e8
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-12-10 10:14:06 -0700

    Use ListBox::selected() in ComboBox::selectionTag()

--- OPHD/UI/Core/ComboBox.cpp
+++ OPHD/UI/Core/ComboBox.cpp
@@ -169,7 +169,7 @@ const std::string& ComboBox::selectionText() const
  */
 int ComboBox::selectionTag() const
 {
-	return lstItems.selectionTag();
+	return lstItems.isItemSelected() ? lstItems.selected().tag : 0;
 }
 
 
