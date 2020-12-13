9fd6b60f738c5fefb8d7cbeb5fbe88d2943a7d16
Daniel Stevens <Dan.R.Stevens@gmail.com>
2020-12-13 16:45:52 -0700

    Regroup text values with text drawing

--- OPHD/UI/Core/ListBox.cpp
+++ OPHD/UI/Core/ListBox.cpp
@@ -17,9 +17,6 @@ using namespace NAS2D;
 
 void ListBox::ListBoxItem::draw(NAS2D::Renderer& renderer, NAS2D::Rectangle<int> drawArea, const Context& context, bool isSelected, bool isHighlighted)
 {
-	const auto textPosition = drawArea.startPoint() + NAS2D::Vector{constants::MARGIN_TIGHT, 0};
-	const auto textColor = isHighlighted ? context.textColorMouseHover : context.textColorNormal;
-
 	// Draw background rect
 	const auto backgroundColor = isSelected ? context.backgroundColorSelected : context.backgroundColorNormal;
 	renderer.drawBoxFilled(drawArea, backgroundColor);
@@ -31,6 +28,8 @@ void ListBox::ListBoxItem::draw(NAS2D::Renderer& renderer, NAS2D::Rectangle<int>
 	}
 
 	// Draw item contents
+	const auto textPosition = drawArea.startPoint() + NAS2D::Vector{constants::MARGIN_TIGHT, 0};
+	const auto textColor = isHighlighted ? context.textColorMouseHover : context.textColorNormal;
 	renderer.drawTextShadow(context.font, text, textPosition, {1, 1}, textColor, NAS2D::Color::Black);
 }
 
