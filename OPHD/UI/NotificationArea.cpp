4ab12eb786f32e76aca439edf3c2e3d5013aa73e
ldicker83 <leeor_net@yahoo.com>
2021-03-29 17:28:32 -0400

    Add draw logic to NotificationArea that draws notifications from bottom to top

--- OPHD/UI/NotificationArea.cpp
+++ OPHD/UI/NotificationArea.cpp
@@ -1,6 +1,7 @@
 #include "NotificationArea.h"
 
 #include "../Cache.h"
+#include "../Constants/UiConstants.h"
 
 #include <NAS2D/Utility.h>
 #include <NAS2D/Renderer/Renderer.h>
@@ -45,13 +46,15 @@ NotificationArea::~NotificationArea()
 
 void NotificationArea::push(const std::string& message, NotificationType type)
 {
-	mNotificationList.emplace_back(Notification{ message, type, Point<int>{0, 0} });
+	mNotificationList.emplace_back(Notification{ message, type });
 }
 
 
 void NotificationArea::onMouseDown(EventHandler::MouseButton button, int x, int y)
 {
+	if (button != EventHandler::MouseButton::Left) { return; }
 
+	NAS2D::Point clickPoint{ x, y };
 }
 
 
@@ -62,10 +65,17 @@ void NotificationArea::update()
 	renderer.drawBox(rect());
 
 	constexpr Rectangle<float> bgRect{128, 64, 32, 32};
+	constexpr int offset = constants::MARGIN_TIGHT + 32;
 
+	const int posX = positionX() + (size().x / 2) - 16;
+
+	int count = 1;
 	for (auto& notification : mNotificationList)
 	{
-		renderer.drawSubImage(mIcons, { 0, 0 }, bgRect, NotificationIconColor.at(notification.type));
-		renderer.drawSubImage(mIcons, { 0, 0 }, NotificationIconRect.at(notification.type), Color::Normal);
+		auto position = Point<int>{ posX, positionY() + size().y - (offset * count) };
+		renderer.drawSubImage(mIcons, position, bgRect, NotificationIconColor.at(notification.type));
+		renderer.drawSubImage(mIcons, position, NotificationIconRect.at(notification.type), Color::Normal);
+
+		count++;
 	}
 }
