6a9176d4b8d356c30ec1ac065a9f6f48d6e4322c
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-06-26 13:33:29 -0600

    Use reference syntax for Robot parameter

--- OPHD/States/MapViewStateHelper.cpp
+++ OPHD/States/MapViewStateHelper.cpp
@@ -562,16 +562,16 @@ void resetTileIndexFromDozer(Robot* robot, Tile* tile)
 // = CONVENIENCE FUNCTIONS FOR WRITING OUT GAME STATE INFORMATION
 // ==============================================================
 
-NAS2D::Dictionary checkRobotDeployment(RobotTileTable& robotTileTable, Robot* robot, Robot::Type type)
+NAS2D::Dictionary checkRobotDeployment(RobotTileTable& robotTileTable, Robot& robot, Robot::Type type)
 {
 	NAS2D::Dictionary dictionary{{
-		{"id", robot->id()},
+		{"id", robot.id()},
 		{"type", static_cast<int>(type)},
-		{"age", robot->fuelCellAge()},
-		{"production", robot->turnsToCompleteTask()},
+		{"age", robot.fuelCellAge()},
+		{"production", robot.turnsToCompleteTask()},
 	}};
 
-	const auto it = robotTileTable.find(robot);
+	const auto it = robotTileTable.find(&robot);
 	if (it != robotTileTable.end())
 	{
 		const auto& tile = *it->second;
@@ -593,20 +593,20 @@ void writeRobots(NAS2D::Xml::XmlElement* element, RobotPool& robotPool, RobotTil
 
 	for (auto digger : robotPool.diggers())
 	{
-		auto dictionary = checkRobotDeployment(robotMap, digger, Robot::Type::Digger);
+		auto dictionary = checkRobotDeployment(robotMap, *digger, Robot::Type::Digger);
 		dictionary.set("direction", static_cast<int>(digger->direction()));
 		robots->linkEndChild(NAS2D::dictionaryToAttributes("robot", dictionary));
 	}
 
 	for (auto dozer : robotPool.dozers())
 	{
-		auto dictionary = checkRobotDeployment(robotMap, dozer, Robot::Type::Dozer);
+		auto dictionary = checkRobotDeployment(robotMap, *dozer, Robot::Type::Dozer);
 		robots->linkEndChild(NAS2D::dictionaryToAttributes("robot", dictionary));
 	}
 
 	for (auto miner : robotPool.miners())
 	{
-		auto dictionary = checkRobotDeployment(robotMap, miner, Robot::Type::Miner);
+		auto dictionary = checkRobotDeployment(robotMap, *miner, Robot::Type::Miner);
 		robots->linkEndChild(NAS2D::dictionaryToAttributes("robot", dictionary));
 	}
 
