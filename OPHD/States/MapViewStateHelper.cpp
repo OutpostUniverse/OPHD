OutpostUniverse/OPHD

commit ab950f0a434e6993243bd0eb4c32d4bab6f76306

Factor out common connectorDirection checks

OPHD/States/MapViewStateHelper.cpp
@@ -96,9 +96,10 @@ bool checkStructurePlacement(Tile& tile, Direction dir)
 	}
 
 	const auto connectorDirection = structure->connectorDirection();
-	return (dir == Direction::East || dir == Direction::West) ?
-		(connectorDirection == ConnectorDir::CONNECTOR_INTERSECTION || connectorDirection == ConnectorDir::CONNECTOR_RIGHT || connectorDirection == ConnectorDir::CONNECTOR_VERTICAL) :
-		(connectorDirection == ConnectorDir::CONNECTOR_INTERSECTION || connectorDirection == ConnectorDir::CONNECTOR_LEFT || connectorDirection == ConnectorDir::CONNECTOR_VERTICAL);
+	return (connectorDirection == ConnectorDir::CONNECTOR_INTERSECTION || connectorDirection == ConnectorDir::CONNECTOR_VERTICAL) ||
+		(dir == Direction::East || dir == Direction::West) ?
+			(connectorDirection == ConnectorDir::CONNECTOR_RIGHT) :  // East/West
+			(connectorDirection == ConnectorDir::CONNECTOR_LEFT);  // North/South
 }
 
 

commit 600c6dc696f2b9b6776f6f41bd0845912e86663e

Add parenthesis to fix associativity problem
Fixes bug introduced in ab950f0a434e6993243bd0eb4c32d4bab6f76306.

OPHD/States/MapViewStateHelper.cpp
@@ -81,9 +81,9 @@ bool checkStructurePlacement(Tile& tile, Direction dir)
 
 	const auto connectorDirection = structure->connectorDirection();
 	return (connectorDirection == ConnectorDir::CONNECTOR_INTERSECTION || connectorDirection == ConnectorDir::CONNECTOR_VERTICAL) ||
-		(dir == Direction::East || dir == Direction::West) ?
+		((dir == Direction::East || dir == Direction::West) ?
 			(connectorDirection == ConnectorDir::CONNECTOR_RIGHT) :  // East/West
-			(connectorDirection == ConnectorDir::CONNECTOR_LEFT);  // North/South
+			(connectorDirection == ConnectorDir::CONNECTOR_LEFT));  // North/South
 }
 
 