OutpostUniverse/OPHD

commit 36f2070bfa9295647fdac160f29f3183f7e0ff61

Factor out common connectorDirection checks

OPHD/States/MapViewStateHelper.cpp
@@ -50,9 +50,10 @@ bool checkTubeConnection(Tile& tile, Direction dir, ConnectorDir sourceConnector
 
 	if (sourceConnectorDir == ConnectorDir::CONNECTOR_INTERSECTION)
 	{
-		return (dir == Direction::East || dir == Direction::West) ?
-			(connectorDirection == ConnectorDir::CONNECTOR_INTERSECTION || connectorDirection == ConnectorDir::CONNECTOR_RIGHT || connectorDirection == ConnectorDir::CONNECTOR_VERTICAL) :  // East/West
-			(connectorDirection == ConnectorDir::CONNECTOR_INTERSECTION || connectorDirection == ConnectorDir::CONNECTOR_LEFT || connectorDirection == ConnectorDir::CONNECTOR_VERTICAL);  // North/South
+		return (connectorDirection == ConnectorDir::CONNECTOR_INTERSECTION || connectorDirection == ConnectorDir::CONNECTOR_VERTICAL) ||
+			(dir == Direction::East || dir == Direction::West) ?
+				(connectorDirection == ConnectorDir::CONNECTOR_RIGHT) :  // East/West
+				(connectorDirection == ConnectorDir::CONNECTOR_LEFT);  // North/South
 	}
 	else if (sourceConnectorDir == ConnectorDir::CONNECTOR_RIGHT && (dir == Direction::East || dir == Direction::West))
 	{

commit b113e3b2a46dbad63db291ed0f30848fbceb7d82

Add parenthesis to fix associativity problem
Fixes bug introduced in 36f2070bfa9295647fdac160f29f3183f7e0ff61.

OPHD/States/MapViewStateHelper.cpp
@@ -51,9 +51,9 @@ bool checkTubeConnection(Tile& tile, Direction dir, ConnectorDir sourceConnector
 	if (sourceConnectorDir == ConnectorDir::CONNECTOR_INTERSECTION)
 	{
 		return (connectorDirection == ConnectorDir::CONNECTOR_INTERSECTION || connectorDirection == ConnectorDir::CONNECTOR_VERTICAL) ||
-			(dir == Direction::East || dir == Direction::West) ?
+			((dir == Direction::East || dir == Direction::West) ?
 				(connectorDirection == ConnectorDir::CONNECTOR_RIGHT) :  // East/West
-				(connectorDirection == ConnectorDir::CONNECTOR_LEFT);  // North/South
+				(connectorDirection == ConnectorDir::CONNECTOR_LEFT));  // North/South
 	}
 	else if (sourceConnectorDir == ConnectorDir::CONNECTOR_RIGHT && (dir == Direction::East || dir == Direction::West))
 	{