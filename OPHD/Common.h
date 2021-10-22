0913b618c161710b008053e44a332019f4a43191
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-10-22 12:10:24 -0600

    Use std::vector<std::string> instead of NAS2D::StringList

--- OPHD/Common.h
+++ OPHD/Common.h
@@ -317,7 +317,7 @@ bool doYesNoMessage(const std::string& title, const std::string msg);
 void checkSavegameVersion(const std::string& filename);
 NAS2D::Xml::XmlDocument openSavegame(const std::string& filename);
 
-NAS2D::StringList split_string(const char* str, char delim);
+std::vector<std::string> split_string(const char* str, char delim);
 
 /**
  * Translates a ProductType enumerator into a string.
