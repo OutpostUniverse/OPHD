88370c6b68b294671c5f8623f373987fb217bff3
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-10-23 16:25:41 -0600

    Convert cout debug message to exception in UIContainer

--- OPHD/UI/Core/UIContainer.cpp
+++ OPHD/UI/Core/UIContainer.cpp
@@ -60,8 +60,7 @@ void UIContainer::bringToFront(Control* control)
 	auto control_iterator = std::find(mControls.begin(), mControls.end(), control);
 	if (control_iterator == mControls.end())
 	{
-		std::cout << "UIContainer::bringToFront(): Control is not managed by this container." << std::endl; // debug aid, can be pulled in release modes.
-		return;
+		throw std::runtime_error("UIContainer::bringToFront(): Control is not managed by this container.");
 	}
 
 	mControls.back()->hasFocus(false);
