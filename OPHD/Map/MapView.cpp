704f0bf471d98a0cbf8d6606ff80f7f31ce7f3be
Daniel Stevens <Dan.R.Stevens@gmail.com>
2021-11-08 17:25:35 -0700

    Add MapView class extracted from TileMap

---
+++ OPHD/Map/MapView.cpp
@@ -0,0 +1,112 @@
+#include "MapView.h"
+
+#include "TileMap.h"
+#include "../DirectionOffset.h"
+
+#include <NAS2D/ParserHelper.h>
+#include <NAS2D/Xml/XmlElement.h>
+
+#include <algorithm>
+
+
+MapView::MapView(TileMap& tileMap) :
+	mTileMap{tileMap}
+{}
+
+
+Tile& MapView::getTile(NAS2D::Point<int> position)
+{
+	return mTileMap.getTile({position, mOriginTilePosition.z});
+}
+
+
+NAS2D::Rectangle<int> MapView::viewArea() const
+{
+	return {mOriginTilePosition.xy.x, mOriginTilePosition.xy.y, mEdgeLength, mEdgeLength};
+}
+
+
+void MapView::mapViewLocation(const MapCoordinate& position)
+{
+	const auto sizeInTiles = mTileMap.size();
+	mOriginTilePosition.xy = {
+		std::clamp(position.xy.x, 0, sizeInTiles.x - mEdgeLength),
+		std::clamp(position.xy.y, 0, sizeInTiles.y - mEdgeLength)
+	};
+	currentDepth(position.z);
+}
+
+
+void MapView::centerOn(NAS2D::Point<int> point)
+{
+	centerOn({point, mOriginTilePosition.z});
+}
+
+
+void MapView::centerOn(const MapCoordinate& position)
+{
+	mapViewLocation({position.xy - NAS2D::Vector{mEdgeLength, mEdgeLength} / 2, position.z});
+}
+
+
+void MapView::moveView(Direction direction)
+{
+	mapViewLocation({
+		mOriginTilePosition.xy + directionEnumToOffset(direction),
+		mOriginTilePosition.z + directionEnumToVerticalOffset(direction)
+	});
+}
+
+
+void MapView::currentDepth(int i)
+{
+	mOriginTilePosition.z = std::clamp(i, 0, mTileMap.maxDepth());
+}
+
+
+int MapView::viewSize() const
+{
+	return mEdgeLength;
+}
+
+
+void MapView::viewSize(int sizeInTiles)
+{
+	mEdgeLength = std::max(3, sizeInTiles);
+}
+
+
+bool MapView::isVisibleTile(const MapCoordinate& position) const
+{
+	return viewArea().contains(position.xy) && position.z == mOriginTilePosition.z;
+}
+
+
+void MapView::serialize(NAS2D::Xml::XmlElement* element)
+{
+	// ==========================================
+	// VIEW PARAMETERS
+	// ==========================================
+	element->linkEndChild(NAS2D::dictionaryToAttributes(
+		"view_parameters",
+		{{
+			{"currentdepth", mOriginTilePosition.z},
+			{"viewlocation_x", mOriginTilePosition.xy.x},
+			{"viewlocation_y", mOriginTilePosition.xy.y},
+		}}
+	));
+}
+
+
+void MapView::deserialize(NAS2D::Xml::XmlElement* element)
+{
+	// VIEW PARAMETERS
+	auto* viewParameters = element->firstChildElement("view_parameters");
+	const auto dictionary = NAS2D::attributesToDictionary(*viewParameters);
+
+	const auto viewX = dictionary.get<int>("viewlocation_x");
+	const auto viewY = dictionary.get<int>("viewlocation_y");
+	const auto viewDepth = dictionary.get<int>("currentdepth");
+
+	mapViewLocation({{viewX, viewY}, viewDepth});
+}
---
+++ OPHD/Map/MapView.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "../Map/MapCoordinate.h"
+
+#include <NAS2D/Math/Rectangle.h>
+
+
+namespace NAS2D
+{
+	namespace Xml
+	{
+		class XmlElement;
+	}
+}
+
+class Tile;
+class TileMap;
+
+
+class MapView
+{
+public:
+	MapView(TileMap& tileMap);
+
+	Tile& getTile(NAS2D::Point<int> position);
+
+	NAS2D::Rectangle<int> viewArea() const;
+	void mapViewLocation(const MapCoordinate& position);
+	void centerOn(NAS2D::Point<int> point);
+	void centerOn(const MapCoordinate& position);
+	void moveView(Direction direction);
+
+	int currentDepth() const { return mOriginTilePosition.z; }
+	void currentDepth(int i);
+
+	int viewSize() const;
+	void viewSize(int sizeInTiles);
+
+	bool isVisibleTile(const MapCoordinate& position) const;
+
+	void serialize(NAS2D::Xml::XmlElement* element);
+	void deserialize(NAS2D::Xml::XmlElement* element);
+
+private:
+	TileMap& mTileMap;
+	int mEdgeLength = 0;
+	MapCoordinate mOriginTilePosition{{0, 0}, 0}; // Top tile of detail view diamond, or top left corner of minimap view box
+};
