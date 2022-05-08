8ffb7b5c31a60db8f0396da12f814892565bbade
Leeor Dicker <leeor_net@yahoo.com>
2022-05-08 01:49:16 -0400

    Implement StructureTracker functions

--- OPHD/States/StructureTracker.cpp
+++ OPHD/States/StructureTracker.cpp
@@ -2,17 +2,28 @@
 
 namespace
 {
+	void addItemToList(const StructureTracker::StructureItem& structureItem, StructureTracker::StructureItemList& list)
+	{
+		for (const auto& item : list)
+		{
+			if (item.id == structureItem.id)
+			{
+				return;
+			}
+		}
 
+		list.push_back(structureItem);
+	}
 };
 
 
 void StructureTracker::addUnlockedSurfaceStructure(const StructureItem& structureItem)
 {
-
+	addItemToList(structureItem, mUnlockedSurfaceStructures);
 }
 
 
 void StructureTracker::addUnlockedUndergroundStructure(const StructureItem& structureItem)
 {
-
+	addItemToList(structureItem, mUnlockedUndergroundStructures);
 }
