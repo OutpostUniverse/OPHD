e7bfe66761252cd5f2c6f425fe2a5bc766921288
Daniel Stevens <Dan.R.Stevens@gmail.com>
2022-10-12 19:54:57 -0600

    Rename `drawArea` to `drawRect`

--- OPHD/UI/Core/ListBox.cpp
+++ OPHD/UI/Core/ListBox.cpp
@@ -11,20 +11,20 @@ unsigned int ListBoxItemText::Context::itemHeight() const
 }
 
 
-void ListBoxItemText::draw(NAS2D::Renderer& renderer, NAS2D::Rectangle<int> drawArea, const Context& context, bool isSelected, bool isHighlighted) const
+void ListBoxItemText::draw(NAS2D::Renderer& renderer, NAS2D::Rectangle<int> drawRect, const Context& context, bool isSelected, bool isHighlighted) const
 {
 	// Draw background rect
 	const auto backgroundColor = isSelected ? context.backgroundColorSelected : context.backgroundColorNormal;
-	renderer.drawBoxFilled(drawArea, backgroundColor);
+	renderer.drawBoxFilled(drawRect, backgroundColor);
 
 	// Draw highlight on mouse over
 	if (isHighlighted)
 	{
-		renderer.drawBox(drawArea, context.itemBorderColorMouseHover);
+		renderer.drawBox(drawRect, context.itemBorderColorMouseHover);
 	}
 
 	// Draw item contents
-	const auto textPosition = drawArea.startPoint() + NAS2D::Vector{constants::MarginTight, 0};
+	const auto textPosition = drawRect.startPoint() + NAS2D::Vector{constants::MarginTight, 0};
 	const auto textColor = isHighlighted ? context.textColorMouseHover : context.textColorNormal;
 	renderer.drawTextShadow(context.font, text, textPosition, {1, 1}, textColor, NAS2D::Color::Black);
 }
