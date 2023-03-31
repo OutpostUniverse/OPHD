c1cfe95aebd987e6e49663b904e44470ed80c2b8
Leeor Dicker <ldicker83@users.noreply.github.com>
2023-03-31 01:16:38 -0400

    Add Xcode project and associated files

---
+++ OutpostHD/OutpostHD/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OutpostHD
+//
+//  Created by Leeor Dicker on 3/30/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
