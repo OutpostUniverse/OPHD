21d01289ee7e001f3df58c51775c34c32fe3a998
Leeor Dicker <leeor_net@yahoo.com>
2023-04-03 22:18:18 -0400

    Add ResearchReport files

---
+++ OPHD/UI/Reports/ResearchReport.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "ReportInterface.h"
+
+#include "../ProductListBox.h"
+#include "../StructureListBox.h"
+
+#include "../../Things/Structures/ResearchFacility.h"
+
+#include "../Core/Button.h"
+
+#include <NAS2D/Math/Point.h>
+
+#include <vector>
+
+
+namespace NAS2D
+{
+	class Font;
+	class Image;
+}
+
+class Warehouse;
+class Structure;
+
+
+class ResearchReport : public ReportInterface
+{
+public:
+	ResearchReport();
+	~ResearchReport() override;
+
+	void fillLists() override;
+	void clearSelected() override;
+
+	void refresh() override;
+	void selectStructure(Structure*) override;
+
+	void update() override;
+
+private:
+	void computeTotalWarehouseCapacity();
+
+	void fillListFromStructureList(const std::vector<ResearchFacility*>&);
+
+	void onResize() override;
+
+	void onDisabled();
+
+	void onTakeMeThere();
+
+	void onStructureSelectionChange();
+
+	void filterButtonClicked();
+
+private:
+	const NAS2D::Font& fontMedium;
+	const NAS2D::Font& fontMediumBold;
+	const NAS2D::Font& fontBigBold;
+
+	const NAS2D::Image& imageLab;
+
+	Button btnTakeMeThere;
+
+	StructureListBox lstStructures;
+	ProductListBox lstProducts;
+
+	std::size_t mLabCount{};
+
+	ResearchFacility* mSelectedLab{ nullptr };
+};
