7c5afafca04132c5462df71699f1ab41fb44ec13
Leeor Dicker <leeor.net@yahoo.com>
2023-04-14 14:57:42 -0400

    Use win32 API ShellExecute for opening explorer window

--- OPHD/ShellOpenPath.cpp
+++ OPHD/ShellOpenPath.cpp
@@ -8,7 +8,8 @@
 namespace
 {
 	#if defined(_WIN32)
-		constexpr const std::string_view ShellOpenCommand{"start"};
+		#include <Windows.h>
+		#include <shellapi.h>
 	#elif defined(__APPLE__)
 		constexpr const std::string_view ShellOpenCommand{"open"};
 	#elif defined(__linux__) || defined(__FreeBSD__)
@@ -21,6 +22,10 @@ namespace
 
 void shellOpenPath(const std::string& path)
 {
-	// Explicitly ignore implementation defined return value
-	std::ignore = std::system((std::string{ShellOpenCommand} + " " + path).c_str());
+	#if defined(_WIN32)
+		std::ignore = ShellExecute(NULL, L"explore", std::wstring(path.begin(), path.end()).c_str(), NULL, NULL, SW_NORMAL);
+	#else
+		// Explicitly ignore implementation defined return value
+		std::ignore = std::system((std::string{ShellOpenCommand} + " " + path).c_str());
+	#endif
 }
