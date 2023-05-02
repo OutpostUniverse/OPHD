785bd7a0385a97091700dcfb2454b5993dea1353
Daniel Stevens <Dan.R.Stevens@gmail.com>
2023-05-01 20:48:32 -0600

    Convert space indentation to tabs

--- OPHD/DirectionOffset.cpp
+++ OPHD/DirectionOffset.cpp
@@ -6,25 +6,25 @@
 
 namespace
 {
-    const std::map<Direction, NAS2D::Vector<int>> DirectionOffsetTable =
-    {
-        { Direction::North, DirectionNorth },
-        { Direction::East, DirectionEast },
-        { Direction::West, DirectionWest },
-        { Direction::South, DirectionSouth },
-        { Direction::NorthEast, DirectionNorthEast },
-        { Direction::NorthWest, DirectionNorthWest },
-        { Direction::SouthEast, DirectionSouthEast },
-        { Direction::SouthWest, DirectionSouthWest },
-        { Direction::Up, DirectionCenter },
-        { Direction::Down, DirectionCenter }
-    };
+	const std::map<Direction, NAS2D::Vector<int>> DirectionOffsetTable =
+	{
+		{ Direction::North, DirectionNorth },
+		{ Direction::East, DirectionEast },
+		{ Direction::West, DirectionWest },
+		{ Direction::South, DirectionSouth },
+		{ Direction::NorthEast, DirectionNorthEast },
+		{ Direction::NorthWest, DirectionNorthWest },
+		{ Direction::SouthEast, DirectionSouthEast },
+		{ Direction::SouthWest, DirectionSouthWest },
+		{ Direction::Up, DirectionCenter },
+		{ Direction::Down, DirectionCenter }
+	};
 }
 
 
 NAS2D::Vector<int> directionEnumToOffset(Direction direction)
 {
-    return DirectionOffsetTable.at(direction);
+	return DirectionOffsetTable.at(direction);
 }
 
 
