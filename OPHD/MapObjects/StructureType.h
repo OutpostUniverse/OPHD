26915c43e34b8a9c227c637aeacb29a4c3389eab
Daniel Stevens <Dan.R.Stevens@gmail.com>
2023-05-12 07:33:51 -0600

    Add `StructureType` definition

---
+++ OPHD/MapObjects/StructureType.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "../StorableResources.h"
+#include "../PopulationPool.h"
+
+
+struct StructureType
+{
+	std::string name;
+	std::string spritePath;
+
+	StorableResources buildCost{};
+	StorableResources operationalCost{};
+	PopulationRequirements populationRequirements{};
+
+	int priority{0};
+
+	int turnsToBuild{0};
+	int maxAge{0};
+	int energyRequired{0};
+	int energyProduced{0};
+	int foodProduced{0};
+	int foodStorageCapacity{0};
+	int oreStorageCapacity{0};
+	int integrityDecayRate{1};
+
+	bool isSelfSustained{false};
+	bool isRepairable{true};
+	bool isChapRequired{true};
+	bool isCrimeTarget{false};
+};
