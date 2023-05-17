1d35b910b765c8ad6af7fe91e1df7a4cb21d6ea6
Daniel Stevens <Dan.R.Stevens@gmail.com>
2023-05-17 08:31:30 -0600

    Move `Robodigger` method implementation to .cpp

---
+++ OPHD/MapObjects/Robots/Robodigger.cpp
@@ -0,0 +1,31 @@
+#include "Robodigger.h"
+
+#include "../../Common.h"
+#include "../../Constants/Strings.h"
+
+
+Robodigger::Robodigger() :
+	Robot(constants::Robodigger, "robots/robodigger.sprite", Robot::Type::Digger),
+	mDirection(Direction::Down)
+{
+}
+
+
+void Robodigger::direction(Direction dir)
+{
+	mDirection = dir;
+}
+
+
+Direction Robodigger::direction() const
+{
+	return mDirection;
+}
+
+
+NAS2D::Dictionary Robodigger::getDataDict() const
+{
+	auto dictionary = Robot::getDataDict();
+	dictionary.set("direction", static_cast<int>(mDirection));
+	return dictionary;
+}
