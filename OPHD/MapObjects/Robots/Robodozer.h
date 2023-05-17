0c1545a1706865690dfac623de45ca6076d407d9
Daniel Stevens <Dan.R.Stevens@gmail.com>
2023-05-17 08:39:33 -0600

    Move `Robodozer` method implementation to .cpp

--- OPHD/MapObjects/Robots/Robodozer.h
+++ OPHD/MapObjects/Robots/Robodozer.h
@@ -2,29 +2,16 @@
 
 #include "../Robot.h"
 
-#include "../../Constants/Strings.h"
-#include "../../Map/Tile.h"
+class Tile;
 
 
 class Robodozer : public Robot
 {
 public:
-	Robodozer(): Robot(constants::Robodozer, "robots/robodozer.sprite", Robot::Type::Dozer)
-	{
-	}
+	Robodozer();
 
-	void startTask(Tile& tile) override
-	{
-		Robot::startTask(tile);
-
-		mTileIndex = static_cast<std::size_t>(tile.index());
-		tile.index(TerrainType::Dozed);
-	}
-
-	void abortTask(Tile& tile) override
-	{
-		tile.index(static_cast<TerrainType>(mTileIndex));
-	}
+	void startTask(Tile& tile) override;
+	void abortTask(Tile& tile) override;
 
 private:
 	std::size_t mTileIndex = 0;
