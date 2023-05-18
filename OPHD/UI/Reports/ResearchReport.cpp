47abdbc6844396bdf9f1feac6b0b961b229b76bb
Leeor Dicker <leeor_net@yahoo.com>
2023-05-17 22:10:18 -0400

    Stub out ResearchReport

--- OPHD/UI/Reports/ResearchReport.cpp
+++ OPHD/UI/Reports/ResearchReport.cpp
@@ -11,7 +11,10 @@ ResearchReport::ResearchReport():
 	fontMedium{fontCache.load(constants::FONT_PRIMARY, constants::FontPrimaryMedium)},
 	fontMediumBold{fontCache.load(constants::FONT_PRIMARY_BOLD, constants::FontPrimaryMedium)},
 	fontBigBold{fontCache.load(constants::FONT_PRIMARY_BOLD, constants::FontPrimaryHuge)},
-	imageLab{imageCache.load("ui/interface/lab_ug.png")}
+	imageLab{imageCache.load("ui/interface/lab_ug.png")},
+	imageUiIcons{imageCache.load("ui/icons.png")},
+	imageCategoryIcons{imageCache.load("categoryicons.png")},
+	imageTopicIcons{imageCache.load("topicicons.png")}
 {
 }
 
@@ -33,6 +36,7 @@ void ResearchReport::clearSelected()
 
 void ResearchReport::refresh()
 {
+	// determine icon placements here
 }
 
 
@@ -50,7 +54,16 @@ void ResearchReport::injectTechReferences(TechnologyCatalog& catalog, ResearchTr
 
 void ResearchReport::update()
 {
+	drawReport();
+}
+
+
+void ResearchReport::drawReport()
+{
 	auto& renderer = NAS2D::Utility<NAS2D::Renderer>::get();
 
 	renderer.drawImage(imageLab, rect().startPoint() + NAS2D::Vector<int>{10, 10});
+
+	renderer.drawImage(imageCategoryIcons, {100, 100});
+	renderer.drawImage(imageTopicIcons, {200, 200});
 }
