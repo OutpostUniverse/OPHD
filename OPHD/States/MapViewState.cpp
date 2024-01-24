4d45645934ccd8c091c32fbcd79d3f2bf0e38a10
Alexander Hunt <alexander.l.hunt951@gmail.com>
2024-01-24 10:15:05 -0500

    Implemented Shift Move

--- OPHD/States/MapViewState.cpp
+++ OPHD/States/MapViewState.cpp
@@ -412,26 +412,29 @@ void MapViewState::onKeyDown(NAS2D::EventHandler::KeyCode key, NAS2D::EventHandl
 		return;
 	}
 
+	const int shiftMoveScalar = NAS2D::EventHandler::shift(mod) ? 5 : 1;
+
 	switch(key)
 	{
+
 		case NAS2D::EventHandler::KeyCode::KEY_w:
 		case NAS2D::EventHandler::KeyCode::KEY_UP:
-			mMapView->moveView(MapOffsetNorthWest);
+			mMapView->moveView(MapOffset{DirectionNorthWest * shiftMoveScalar, 0});
 			break;
 
 		case NAS2D::EventHandler::KeyCode::KEY_s:
 		case NAS2D::EventHandler::KeyCode::KEY_DOWN:
-			mMapView->moveView(MapOffsetSouthEast);
+			mMapView->moveView(MapOffset{DirectionSouthEast * shiftMoveScalar, 0});
 			break;
 
 		case NAS2D::EventHandler::KeyCode::KEY_a:
 		case NAS2D::EventHandler::KeyCode::KEY_LEFT:
-			mMapView->moveView(MapOffsetSouthWest);
+			mMapView->moveView(MapOffset{DirectionSouthWest * shiftMoveScalar, 0});
 			break;
 
 		case NAS2D::EventHandler::KeyCode::KEY_d:
 		case NAS2D::EventHandler::KeyCode::KEY_RIGHT:
-			mMapView->moveView(MapOffsetNorthEast);
+			mMapView->moveView(MapOffset{DirectionNorthEast * shiftMoveScalar, 0});
 			break;
 
 		case NAS2D::EventHandler::KeyCode::KEY_0:
