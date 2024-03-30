86337b44ce5ab5bb93df07ad7aa2b38d0b7e62dc
Alex <alexander.l.hunt951@gmail.com>
2024-03-30 09:38:35 -0400

    Migrated mMapView->moveView()s  to KeyboardInputHandler

--- OPHD/States/MapViewState.cpp
+++ OPHD/States/MapViewState.cpp
@@ -416,31 +416,10 @@ void MapViewState::onKeyDown(NAS2D::EventHandler::KeyCode key, NAS2D::EventHandl
 		return;
 	}
 
-	const int shiftMoveScalar = NAS2D::EventHandler::shift(mod) ? 5 : 1;
+	mKeyboardInputHandler.handleInput(key, mod);
 
 	switch(key)
 	{
-
-		case NAS2D::EventHandler::KeyCode::KEY_w:
-		case NAS2D::EventHandler::KeyCode::KEY_UP:
-			mMapView->moveView(MapOffset{DirectionNorthWest * shiftMoveScalar, 0});
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_s:
-		case NAS2D::EventHandler::KeyCode::KEY_DOWN:
-			mMapView->moveView(MapOffset{DirectionSouthEast * shiftMoveScalar, 0});
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_a:
-		case NAS2D::EventHandler::KeyCode::KEY_LEFT:
-			mMapView->moveView(MapOffset{DirectionSouthWest * shiftMoveScalar, 0});
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_d:
-		case NAS2D::EventHandler::KeyCode::KEY_RIGHT:
-			mMapView->moveView(MapOffset{DirectionNorthEast * shiftMoveScalar, 0});
-			break;
-
 		case NAS2D::EventHandler::KeyCode::KEY_0:
 			changeViewDepth(0);
 			break;
