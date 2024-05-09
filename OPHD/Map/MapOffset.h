8ddea407e7257a4a190fbf5e78c82e211b44b0ea
Alex <alexander.l.hunt951@gmail.com>
2024-05-09 08:31:53 -0400

    Added operators overloads for MapOffset

--- OPHD/Map/MapOffset.h
+++ OPHD/Map/MapOffset.h
@@ -8,3 +8,35 @@ struct MapOffset
 	NAS2D::Vector<int> xy;
 	int z;
 };
+
+
+inline bool operator==(const MapOffset& offset1, const MapOffset& offset2)
+{
+	return offset1.xy == offset2.xy && offset1.z == offset2.z;
+}
+
+
+inline bool operator!=(const MapOffset& offset1, const MapOffset& offset2)
+{
+	return !(offset1 == offset2);
+}
+
+
+inline MapOffset& operator*=(MapOffset& offset, int scalar)
+{
+	offset.xy *= scalar;
+	offset.z *= scalar;
+	return offset;
+}
+
+
+inline MapOffset operator*(MapOffset offset, int scalar)
+{
+	return offset *= scalar;
+}
+
+
+inline MapOffset operator*(int scalar, MapOffset offset)
+{
+	return offset *= scalar;
+}
