acbd1ba20ee7e8d70b33a71a0baa90f683049e85
Alex <alexander.l.hunt951@gmail.com>
2024-07-25 09:07:07 -0400

    Updated  MapViewState::onKeyDown to use mKeyCodeMap and commands

--- OPHD/States/MapViewState.cpp
+++ OPHD/States/MapViewState.cpp
@@ -458,105 +458,25 @@ void MapViewState::onKeyDown(NAS2D::EventHandler::KeyCode key, NAS2D::EventHandl
 		return;
 	}
 
-	if (key == NAS2D::EventHandler::KeyCode::KEY_F1)
+	auto filteredKeyModifier = NAS2D::EventHandler::KeyModifier::None;
+
+	if (NAS2D::EventHandler::control(mod))
 	{
-		mReportsUiSignal();
-		return;
+		filteredKeyModifier = filteredKeyModifier | NAS2D::EventHandler::KeyModifier::Ctrl;
+	}
+	if (NAS2D::EventHandler::alt(mod))
+	{
+		filteredKeyModifier = filteredKeyModifier | NAS2D::EventHandler::KeyModifier::Alt;
+	}
+	if (NAS2D::EventHandler::shift(mod))
+	{
+		filteredKeyModifier = filteredKeyModifier | NAS2D::EventHandler::KeyModifier::Shift;
 	}
 
-	const int shiftMoveScalar = NAS2D::EventHandler::shift(mod) ? 5 : 1;
-
-	switch(key)
+	auto commandIterator = mKeyCodeMap.find({filteredKeyModifier, key});
+	if (commandIterator != mKeyCodeMap.end())
 	{
-
-		case NAS2D::EventHandler::KeyCode::KEY_w:
-		case NAS2D::EventHandler::KeyCode::KEY_UP:
-			moveView(MapOffsetNorthWest * shiftMoveScalar);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_s:
-		case NAS2D::EventHandler::KeyCode::KEY_DOWN:
-			moveView(MapOffsetSouthEast * shiftMoveScalar);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_a:
-		case NAS2D::EventHandler::KeyCode::KEY_LEFT:
-			moveView(MapOffsetSouthWest * shiftMoveScalar);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_d:
-		case NAS2D::EventHandler::KeyCode::KEY_RIGHT:
-			moveView(MapOffsetNorthEast * shiftMoveScalar);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_0:
-			changeViewDepth(0);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_1:
-			changeViewDepth(1);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_2:
-			changeViewDepth(2);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_3:
-			changeViewDepth(3);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_4:
-			changeViewDepth(4);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_PAGEUP:
-			moveView(MapOffsetUp);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_PAGEDOWN:
-			moveView(MapOffsetDown);
-			break;
-
-
-		case NAS2D::EventHandler::KeyCode::KEY_HOME:
-			changeViewDepth(0);
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_END:
-			changeViewDepth(mTileMap->maxDepth());
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_F10:
-			if (NAS2D::Utility<NAS2D::EventHandler>::get().control(mod) && NAS2D::Utility<NAS2D::EventHandler>::get().shift(mod))
-			{
-				mCheatMenu.show();
-				mWindowStack.bringToFront(&mCheatMenu);
-			}
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_F2:
-			mFileIoDialog.scanDirectory(constants::SaveGamePath);
-			mFileIoDialog.setMode(FileIo::FileOperation::Save);
-			mFileIoDialog.show();
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_F3:
-			mFileIoDialog.scanDirectory(constants::SaveGamePath);
-			mFileIoDialog.setMode(FileIo::FileOperation::Load);
-			mFileIoDialog.show();
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_ESCAPE:
-			clearMode();
-			resetUi();
-			break;
-
-		case NAS2D::EventHandler::KeyCode::KEY_ENTER:
-			if (mBtnTurns.enabled()) { nextTurn(); }
-			break;
-
-		default:
-			break;
+		commandIterator->second(*this);
 	}
 }
 
