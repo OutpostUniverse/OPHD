3f35b9dc32bcdc45491bcb34600ffc36c481a529
Alex <alexander.l.hunt951@gmail.com>
2024-12-09 10:22:57 -0500

    Added Morale Class

---
+++ libOPHD/Population/Morale.cpp
@@ -0,0 +1,44 @@
+#include "Morale.h"
+#include "../OPHD/Constants/Numbers.h"
+
+#include <algorithm>
+
+
+Morale::Morale() :
+	Morale(constants::DefaultStartingMorale,constants::DefaultStartingMorale)
+{
+}
+
+
+Morale::Morale(int currentMorale, int previousMorale) :
+	mCurrentMorale{currentMorale},
+	mPreviousMorale{previousMorale},
+	mMoraleAccumulator{0}
+{
+}
+
+
+int Morale::currentMorale() const
+{
+	return std::clamp(mCurrentMorale, 0, constants::MaximumMorale);
+}
+
+
+int Morale::previousMorale() const
+{
+	return std::clamp(mPreviousMorale, 0, constants::MaximumMorale);
+}
+
+
+void Morale::adjustMorale(int diff)
+{
+	mMoraleAccumulator += diff;
+}
+
+
+void Morale::commitMoraleChanges()
+{
+	mPreviousMorale = currentMorale();
+	mCurrentMorale = std::clamp(mCurrentMorale + mMoraleAccumulator, 0, constants::MaximumMorale);
+	mMoraleAccumulator = 0;
+}
