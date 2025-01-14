9b17cf22871aa0c67df9813d32c3a2b3e1775af9
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-01-14 13:40:08 -0700

    Split solar structure implementations to source files
    This reduces the reach of the `include` for `Common.h`.
    
    This reduces the number of Clang warnings for `-Wweak-vtables`.

--- OPHD/MapObjects/Structures/SolarPanelArray.h
+++ OPHD/MapObjects/Structures/SolarPanelArray.h
@@ -2,28 +2,12 @@
 
 #include "PowerStructure.h"
 
-#include "../../Common.h"
-#include "../../Constants/Strings.h"
-
-
-const int SOLAR_PANEL_BASE_PRODUCUCTION = 50;
-
 
 class SolarPanelArray : public PowerStructure
 {
 public:
-	SolarPanelArray() :
-		PowerStructure
-		{
-			StructureClass::EnergyProduction,
-			StructureID::SID_SOLAR_PANEL1
-		}
-	{
-	}
+	SolarPanelArray();
 
 protected:
-	int calculateMaxEnergyProduction() override
-	{
-		return static_cast<int>(SOLAR_PANEL_BASE_PRODUCUCTION / getMeanSolarDistance());
-	}
+	int calculateMaxEnergyProduction() override;
 };
--- OPHD/MapObjects/Structures/SolarPlant.h
+++ OPHD/MapObjects/Structures/SolarPlant.h
@@ -2,28 +2,12 @@
 
 #include "PowerStructure.h"
 
-#include "../../Common.h"
-#include "../../Constants/Strings.h"
-
-
-const int SOLAR_PLANT_BASE_PRODUCUCTION = 2000;
-
 
 class SolarPlant : public PowerStructure
 {
 public:
-	SolarPlant() :
-		PowerStructure
-		{
-			StructureClass::EnergyProduction,
-			StructureID::SID_SOLAR_PLANT
-		}
-	{
-	}
+	SolarPlant();
 
 protected:
-	int calculateMaxEnergyProduction() override
-	{
-		return static_cast<int>(SOLAR_PLANT_BASE_PRODUCUCTION / getMeanSolarDistance());
-	}
+	int calculateMaxEnergyProduction() override;
 };
