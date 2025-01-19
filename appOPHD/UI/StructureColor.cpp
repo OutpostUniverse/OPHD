e1e1a5c06303ed53c06ec6d71b5f0c059c0ab8c5
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-01-18 20:01:53 -0700

    Remove `using namespace NAS2D;` in `StructureColor.cpp`

--- appOPHD/UI/StructureColor.cpp
+++ appOPHD/UI/StructureColor.cpp
@@ -4,36 +4,34 @@
 
 #include <map>
 
-using namespace NAS2D;
 
-
-const std::map<StructureState, Color> STRUCTURE_COLOR_TABLE
+const std::map<StructureState, NAS2D::Color> STRUCTURE_COLOR_TABLE
 {
-	{StructureState::UnderConstruction, Color{150, 150, 150, 100}},
-	{StructureState::Operational, Color{0, 185, 0}},
-	{StructureState::Idle, Color{0, 185, 0, 100}},
-	{StructureState::Disabled, Color{220, 0, 0}},
-	{StructureState::Destroyed, Color{220, 0, 0}}
+	{StructureState::UnderConstruction, NAS2D::Color{150, 150, 150, 100}},
+	{StructureState::Operational, NAS2D::Color{0, 185, 0}},
+	{StructureState::Idle, NAS2D::Color{0, 185, 0, 100}},
+	{StructureState::Disabled, NAS2D::Color{220, 0, 0}},
+	{StructureState::Destroyed, NAS2D::Color{220, 0, 0}}
 };
 
 
-const std::map<StructureState, Color> STRUCTURE_TEXT_COLOR_TABLE
+const std::map<StructureState, NAS2D::Color> STRUCTURE_TEXT_COLOR_TABLE
 {
-	{StructureState::UnderConstruction, Color{185, 185, 185, 100}},
-	{StructureState::Operational, Color{0, 185, 0}},
-	{StructureState::Idle, Color{0, 185, 0, 100}},
-	{StructureState::Disabled, Color{220, 0, 0}},
-	{StructureState::Destroyed, Color{220, 0, 0}}
+	{StructureState::UnderConstruction, NAS2D::Color{185, 185, 185, 100}},
+	{StructureState::Operational, NAS2D::Color{0, 185, 0}},
+	{StructureState::Idle, NAS2D::Color{0, 185, 0, 100}},
+	{StructureState::Disabled, NAS2D::Color{220, 0, 0}},
+	{StructureState::Destroyed, NAS2D::Color{220, 0, 0}}
 };
 
 
-Color structureColorFromIndex(StructureState structureState)
+NAS2D::Color structureColorFromIndex(StructureState structureState)
 {
 	return STRUCTURE_COLOR_TABLE.at(structureState);
 }
 
 
-Color structureTextColorFromIndex(StructureState structureState)
+NAS2D::Color structureTextColorFromIndex(StructureState structureState)
 {
 	return STRUCTURE_TEXT_COLOR_TABLE.at(structureState);
 }
