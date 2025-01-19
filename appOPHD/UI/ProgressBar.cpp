23415e8705197033d9e1bd6f15aa12d9c76c86e5
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-01-18 19:08:14 -0700

    Remove need for `using namespace NAS2D` in `ProgressBar.cpp`

--- appOPHD/UI/ProgressBar.cpp
+++ appOPHD/UI/ProgressBar.cpp
@@ -4,13 +4,11 @@
 #include <NAS2D/Renderer/Renderer.h>
 #include <NAS2D/Renderer/Color.h>
 
-using namespace NAS2D;
-
 
 void drawProgressBar(int value, int max, NAS2D::Rectangle<int> rect, int padding)
 {
 	const auto clippedValue = std::clamp(value, 0, max);
-	auto& renderer = Utility<Renderer>::get();
+	auto& renderer = NAS2D::Utility<NAS2D::Renderer>::get();
 	renderer.drawBox(rect, NAS2D::Color{0, 185, 0});
 
 	if (max > 0)
