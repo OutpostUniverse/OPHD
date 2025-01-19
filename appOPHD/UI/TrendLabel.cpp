ff31cbbcfd9b3279c3407fea6c0466c5c4a7d2e6
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-01-19 00:32:21 -0700

    Add function `drawTrendLabel`

--- appOPHD/UI/TrendLabel.cpp
+++ appOPHD/UI/TrendLabel.cpp
@@ -1,5 +1,22 @@
 #include "TrendLabel.h"
 
+#include "../Constants/UiConstants.h"
+
+#include <NAS2D/Renderer/Color.h>
+#include <NAS2D/Renderer/Renderer.h>
+
+#include <array>
+
+
+namespace
+{
+	const std::array trendColor{
+		NAS2D::Color::Red,
+		NAS2D::Color::White,
+		constants::PrimaryTextColor,
+	};
+}
+
 
 std::size_t trendIndex(int changeValue)
 {
@@ -11,4 +28,10 @@ std::size_t trendIndex(int changeValue)
 std::string formatDiff(int changeValue)
 {
 	return ((changeValue > 0) ? "+" : "") + std::to_string(changeValue);
+}
+
+
+void drawTrendLabel(NAS2D::Renderer& renderer, const NAS2D::Font& font, int changeValue, NAS2D::Point<int> position)
+{
+	renderer.drawText(font, formatDiff(changeValue), position, trendColor[trendIndex(changeValue)]);
 }
--- appOPHD/UI/TrendLabel.h
+++ appOPHD/UI/TrendLabel.h
@@ -3,5 +3,16 @@
 #include <string>
 
 
+namespace NAS2D
+{
+	class Renderer;
+	class Font;
+
+	template <typename BaseType> struct Point;
+}
+
+
 std::size_t trendIndex(int changeValue);
 std::string formatDiff(int changeValue);
+
+void drawTrendLabel(NAS2D::Renderer& renderer, const NAS2D::Font& font, int changeValue, NAS2D::Point<int> position);
