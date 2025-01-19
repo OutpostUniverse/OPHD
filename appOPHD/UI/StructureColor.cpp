e49787faae25b5d63ebcaff0d017f7c04fd6e9a1
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-01-18 20:03:39 -0700

    Rename structure color tables to use camelCase

--- appOPHD/UI/StructureColor.cpp
+++ appOPHD/UI/StructureColor.cpp
@@ -7,7 +7,7 @@
 
 namespace
 {
-	const std::map<StructureState, NAS2D::Color> STRUCTURE_COLOR_TABLE
+	const std::map<StructureState, NAS2D::Color> structureColorTable
 	{
 		{StructureState::UnderConstruction, NAS2D::Color{150, 150, 150, 100}},
 		{StructureState::Operational, NAS2D::Color{0, 185, 0}},
@@ -17,7 +17,7 @@ namespace
 	};
 
 
-	const std::map<StructureState, NAS2D::Color> STRUCTURE_TEXT_COLOR_TABLE
+	const std::map<StructureState, NAS2D::Color> structureTextColorTable
 	{
 		{StructureState::UnderConstruction, NAS2D::Color{185, 185, 185, 100}},
 		{StructureState::Operational, NAS2D::Color{0, 185, 0}},
@@ -30,11 +30,11 @@ namespace
 
 NAS2D::Color structureColorFromIndex(StructureState structureState)
 {
-	return STRUCTURE_COLOR_TABLE.at(structureState);
+	return structureColorTable.at(structureState);
 }
 
 
 NAS2D::Color structureTextColorFromIndex(StructureState structureState)
 {
-	return STRUCTURE_TEXT_COLOR_TABLE.at(structureState);
+	return structureTextColorTable.at(structureState);
 }
