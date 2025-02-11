2ce9c6890f9327274651a5e591c75188af646b35
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-11 14:31:34 -0700

    Split `ResearchFacility` implementation to source file

--- appOPHD/MapObjects/Structures/ResearchFacility.h
+++ appOPHD/MapObjects/Structures/ResearchFacility.h
@@ -2,71 +2,27 @@
 
 #include "../Structure.h"
 
-#include <algorithm>
-#include <cmath>
-
 
 class ResearchFacility : public Structure
 {
 public:
-	ResearchFacility(StructureClass structureClass, StructureID id) :
-		Structure(structureClass, id)
-	{}
+	ResearchFacility(StructureClass structureClass, StructureID id);
 
+	StringTable createInspectorViewTable() override;
 
-	StringTable createInspectorViewTable() override
-	{
-		StringTable stringTable(2, 3);
+	int regularResearchProduced() const;
+	int hotResearchProduced() const;
 
-		stringTable[{0, 0}].text = "Research Produced:";
-		stringTable[{0, 1}].text = "Regular";
-		stringTable[{0, 2}].text = "Hot";
+	int scientistsNeeded() const;
 
-		stringTable[{1, 1}].text = std::to_string(regularResearchProduced());
-		stringTable[{1, 2}].text = std::to_string(hotResearchProduced());
-
-		return stringTable;
-	}
-
-
-	int regularResearchProduced() const
-	{
-		return static_cast<int>(
-			std::floor(static_cast<float>(mActualScientistsEmployed) * mRegularPointsPerScientist));
-	}
-
-
-	int hotResearchProduced() const
-	{
-		return static_cast<int>(
-			std::floor(static_cast<float>(mActualScientistsEmployed) * mHotPointsPerScientist));
-	}
-
-
-	int scientistsNeeded() const
-	{
-		return mMaxScientistsAllowed - mActualScientistsEmployed;
-	}
-
-
-	void assignScientists(int count)
-	{
-		mActualScientistsEmployed = std::clamp(count, 0, mMaxScientistsAllowed);
-	}
-
-
-	int assignedScientists() const
-	{
-		return mActualScientistsEmployed;
-	}
-
+	void assignScientists(int count);
+	int assignedScientists() const;
 
 protected:
-	void maxScientistsAllowed(int count) { mMaxScientistsAllowed = count; }
+	void maxScientistsAllowed(int count);
 
-	void regularPointsPerScientist(float count) { mRegularPointsPerScientist = count; }
-	void hotPointsPerScientist(float count) { mHotPointsPerScientist = count; }
-
+	void regularPointsPerScientist(float count);
+	void hotPointsPerScientist(float count);
 
 private:
 	int mMaxScientistsAllowed{0};
