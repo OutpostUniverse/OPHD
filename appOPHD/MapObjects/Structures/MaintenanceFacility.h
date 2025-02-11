1ae36a73ff4335d79e9063c4dda10cecd31d1fb4
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-11 14:54:51 -0700

    Split `MaintenanceFacility` implementation to source file

--- appOPHD/MapObjects/Structures/MaintenanceFacility.h
+++ appOPHD/MapObjects/Structures/MaintenanceFacility.h
@@ -18,189 +18,36 @@ public:
 	static constexpr int MinimumPersonnel{1};
 
 public:
-	MaintenanceFacility() : Structure(
-		StructureClass::Maintenance,
-		StructureID::SID_MAINTENANCE_FACILITY)
-	{
-	}
+	MaintenanceFacility();
 
+	void resources(const StorableResources& resources);
+	bool suppliesAvailable() const;
 
-	void resources(const StorableResources& resources)
-	{
-		mResources = &resources;
-	}
+	int maintenancePersonnel() const;
+	void addPersonnel();
+	void removePersonnel();
+	int personnel() const;
+	void personnel(int assigned);
+	bool personnelAvailable() const;
 
+	void addPriorityStructure(Structure* structure);
+	void removePriorityStructure(Structure* structure);
+	bool hasPriorityStructures() const;
 
-	bool suppliesAvailable() const
-	{
-		return mMaterialsLevel > 0;
-	}
-
-
-	int maintenancePersonnel() const
-	{
-		return mMaintenancePersonnel;
-	}
-
-
-	void addPersonnel()
-	{
-		mMaintenancePersonnel = std::clamp(mMaintenancePersonnel + 1, MinimumPersonnel, MaximumPersonnel);
-	}
-
-
-	void removePersonnel()
-	{
-		mMaintenancePersonnel = std::clamp(mMaintenancePersonnel - 1, MinimumPersonnel, MaximumPersonnel);
-	}
-
-
-	int personnel() const
-	{
-		return mMaintenancePersonnel;
-	}
-
-
-	void personnel(int assigned)
-	{
-		mMaintenancePersonnel = assigned;
-	}
-
-
-	bool personnelAvailable() const
-	{
-		return mAssignedPersonnel < mMaintenancePersonnel;
-	}
-
-
-	void addPriorityStructure(Structure* structure)
-	{
-		mPriorityList.push_back(structure);
-	}
-
-
-	void removePriorityStructure(Structure* structure)
-	{
-		auto it = std::find(mPriorityList.begin(), mPriorityList.end(), structure);
-		if (it != mPriorityList.end())
-		{
-			mPriorityList.erase(it);
-		}
-	}
-
-
-	bool hasPriorityStructures() const
-	{
-		return !mPriorityList.empty();
-	}
-
-
-	void repairStructures(StructureList& structures)
-	{
-		if (!operational()) { return; }
-
-		if (hasPriorityStructures())
-		{
-			repairPriorityStructures(structures);
-		}
-
-		for (auto* structure : structures)
-		{
-			repairStructure(structure);
-			std::rotate(structures.begin(), structures.begin() + 1, structures.end());
-		}
-	}
-
+	void repairStructures(StructureList& structures);
 
 protected:
-	bool canMakeRepairs() const
-	{
-		return (personnelAvailable() && suppliesAvailable());
-	}
+	bool canMakeRepairs() const;
 
+	void moveStructureToBack(StructureList& structures, Structure* structure);
 
-	void moveStructureToBack(StructureList& structures, Structure* structure)
-	{
-		auto it = std::find(structures.begin(), structures.end(), structure);
-		if (it != structures.end())
-		{
-			structures.erase(it);
-			structures.push_back(structure);
-		}
-	}
+	void repairPriorityStructures(StructureList& structures);
+	void repairStructure(Structure* structure);
 
-
-	void repairPriorityStructures(StructureList& structures)
-	{
-		for (auto* structure : mPriorityList)
-		{
-			if (!canMakeRepairs()) { return; }
-
-			repairStructure(structure);
-
-			moveStructureToBack(structures, structure);
-
-			if (structure->operational())
-			{
-				removePriorityStructure(structure);
-			}
-		}
-	}
-
-
-	void repairStructure(Structure* structure)
-	{
-		if (structure->destroyed() || structure->underConstruction()) { return; }
-
-		if (!canMakeRepairs()) { return; }
-
-		if (structure->disabled() && structure->disabledReason() == DisabledReason::StructuralIntegrity)
-		{
-			--mMaterialsLevel;
-			++mAssignedPersonnel;
-			if (structure->integrity() > 35) // \fixme magic number
-			{
-				structure->integrity(100);
-				structure->enable();
-			}
-			else
-			{
-				structure->integrity(50);
-				addPriorityStructure(structure);
-			}
-		}
-		else if (structure->operational() || structure->isIdle())
-		{
-			--mMaterialsLevel;
-			++mAssignedPersonnel;
-			structure->integrity(100);
-
-			if (structure->structureId() == StructureID::SID_ROAD)
-			{
-				structure->rebuild();
-			}
-		}
-	}
-
-
-	void think() override
-	{
-		if (mMaterialsLevel == MaintenanceSuppliesCapacity) { return; }
-
-		StorableResources maintenanceSuppliesCost{1, 1, 1, 1};
-
-		if (resources() >= maintenanceSuppliesCost)
-		{
-			removeRefinedResources(maintenanceSuppliesCost);
-			mMaterialsLevel = std::clamp(mMaterialsLevel + 1, 0, MaintenanceSuppliesCapacity);
-		}
-
-		mAssignedPersonnel = 0;
-	}
-
+	void think() override;
 
 private:
-	const StorableResources& resources() { return *mResources; }
+	const StorableResources& resources();
 
 	int mMaterialsLevel{0};
 	int mMaintenancePersonnel{MinimumPersonnel};
