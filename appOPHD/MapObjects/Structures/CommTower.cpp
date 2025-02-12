9bb270b8253a6e8a458386fb421596ff92e1707a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-12 02:01:59 -0700

    Split `CommTower` implementation to source file

---
+++ appOPHD/MapObjects/Structures/CommTower.cpp
@@ -0,0 +1,42 @@
+#include "CommTower.h"
+
+
+#include "../../Constants/Strings.h"
+#include "../../Constants/UiConstants.h"
+
+
+namespace
+{
+	const int BaseRange = 10;
+}
+
+
+CommTower::CommTower() : Structure(
+	StructureClass::Communication,
+	StructureID::SID_COMM_TOWER)
+{
+}
+
+
+int CommTower::getRange() const
+{
+	return operational() ? BaseRange : 0;
+}
+
+
+StringTable CommTower::createInspectorViewTable()
+{
+	StringTable stringTable(2, 1);
+
+	stringTable[{0, 0}].text = "Communication Range:";
+
+	auto communicationRange = getRange();
+	stringTable[{1, 0}].text = std::to_string(communicationRange);
+
+	if (communicationRange == 0)
+	{
+		stringTable[{1, 0}].textColor = constants::WarningTextColor;
+	}
+
+	return stringTable;
+}
