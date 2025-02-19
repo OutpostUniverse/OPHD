0153c031a09e242890c2d017123cc7b1dc73c6d4
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-18 23:19:05 -0700

    Split `CargoLander.h` implementation to source file

---
+++ appOPHD/MapObjects/Structures/CargoLander.cpp
@@ -0,0 +1,29 @@
+#include "CargoLander.h"
+
+
+#include "../../Map/Tile.h"
+
+
+CargoLander::CargoLander(Tile* tile) : Structure(
+	StructureClass::Lander,
+	StructureID::SID_CARGO_LANDER),
+	mTile(tile)
+{
+	enable();
+}
+
+
+CargoLander::Signal::Source& CargoLander::deploySignal()
+{
+	return mDeploy;
+}
+
+
+void CargoLander::think()
+{
+	if (age() == turnsToBuild())
+	{
+		mDeploy();
+		mTile->index(TerrainType::Dozed);
+	}
+}
