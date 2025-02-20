9d09cc5ee9c2eb7c0a06dff253598ed0fc577bf1
Alexander Hunt <alexander.l.hunt951@gmail.com>
2025-02-20 07:51:21 -0500

    Added EventBubbler interface

---
+++ appOPHD/UI/EventBubbler.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+class EventBubbler
+{
+public:
+	virtual ~EventBubbler() = default;
+	//virtual bool handleEvent() = 0;
+
+private:	
+	virtual void addChild(EventBubbler&) = 0;
+};
