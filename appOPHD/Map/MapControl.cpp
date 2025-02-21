d15514e34edb210a69bc0a9bff51ec4f643629c3
Alexander Hunt <alexander.l.hunt951@gmail.com>
2025-02-21 08:39:00 -0500

    Connected  MapControl::onKeyDownEvent to eventHandler

--- appOPHD/Map/MapControl.cpp
+++ appOPHD/Map/MapControl.cpp
@@ -1,15 +1,21 @@
 #include "MapControl.h"
 
 #include <NAS2D/Utility.h>
+#include <NAS2D/EventHandler.h>
 
 MapControl::MapControl()
 {
+	auto& eventHandler = NAS2D::Utility<NAS2D::EventHandler>::get();
+	eventHandler.keyDown().connect({this, &MapControl::onKeyDownEvent});
 }
 
 
 MapControl::~MapControl()
 {
+	auto& eventHandler = NAS2D::Utility<NAS2D::EventHandler>::get();
+	eventHandler.keyDown().disconnect({this, &MapControl::onKeyDownEvent});
 }
+
 
 void MapControl::onKeyDownEvent(NAS2D::KeyCode keyCode, NAS2D::KeyModifier keyMod, bool repeat)
 {
