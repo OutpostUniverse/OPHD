230f6f0de0c355ec7f57c7a93565876565ede109
Alexander Hunt <alexander.l.hunt951@gmail.com>
2025-02-21 09:31:44 -0500

    Updated MapControl::MapControl

--- appOPHD/Map/MapControl.cpp
+++ appOPHD/Map/MapControl.cpp
@@ -1,12 +1,18 @@
 #include "MapControl.h"
 
+#include "../UI/CheatMenu.h"
+#include <libControls/WindowStack.h>
+
 #include <NAS2D/Utility.h>
 #include <NAS2D/EventHandler.h>
 
-MapControl::MapControl()
+MapControl::MapControl(CheatMenu& cheatMenu, WindowStack& windowStack) : 
+mCheatMenu{cheatMenu}, 
+mWindowStack{windowStack}
 {
 	auto& eventHandler = NAS2D::Utility<NAS2D::EventHandler>::get();
 	eventHandler.keyDown().connect({this, &MapControl::onKeyDownEvent});
+	addChild(&mCheatMenu);
 }
 
 
--- appOPHD/Map/MapControl.h
+++ appOPHD/Map/MapControl.h
@@ -1,16 +1,20 @@
 #pragma once
 
-#include "../../libControls/Control.h"
+#include <libControls/Control.h>
 
 #include <NAS2D/EnumKeyCode.h>
 #include <NAS2D/EnumKeyModifier.h>
 
+class CheatMenu;
+class WindowStack;
 class MapControl : public Control
 {
 public:
-	MapControl();
+	MapControl(CheatMenu& cheatMenu, WindowStack& windowStack);
 	~MapControl() override;
 
 private:
 	void onKeyDownEvent(NAS2D::KeyCode keyCode, NAS2D::KeyModifier keyMod, bool repeat);
+	CheatMenu& mCheatMenu;
+	WindowStack& mWindowStack;
 };
