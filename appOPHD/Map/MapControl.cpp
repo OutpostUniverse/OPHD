0d2c3b2a11e8676be88cabcdda5516e8f5a237ab
Alexander Hunt <alexander.l.hunt951@gmail.com>
2025-02-21 08:21:46 -0500

    Added MapControl

---
+++ appOPHD/Map/MapControl.cpp
@@ -0,0 +1,12 @@
+#include "MapControl.h"
+
+#include <NAS2D/Utility.h>
+
+MapControl::MapControl()
+{
+}
+
+
+MapControl::~MapControl()
+{
+}
---
+++ appOPHD/Map/MapControl.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../../libControls/Control.h"
+
+class MapControl : public Control
+{
+public:
+	MapControl();
+	~MapControl() override;
+};
