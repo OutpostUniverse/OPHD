294b205ae0fb29846fc65d2076c5fd2d162c33e7
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-23 21:08:13 -0700

    Increase size of view filter buttons

--- appOPHD/UI/Reports/FactoryReport.cpp
+++ appOPHD/UI/Reports/FactoryReport.cpp
@@ -27,7 +27,7 @@ using namespace NAS2D;
 
 namespace
 {
-	constexpr auto viewFilterButtonSize = NAS2D::Vector{75, 20};
+	constexpr auto viewFilterButtonSize = NAS2D::Vector{85, 20};
 	constexpr auto viewFilterSpacing = NAS2D::Vector{viewFilterButtonSize.x + constants::MarginTight, 0};
 	constexpr auto viewFilterOriginRow1 = NAS2D::Vector{10, 10};
 	constexpr auto viewFilterOriginRow2 = NAS2D::Vector{10, 33};
