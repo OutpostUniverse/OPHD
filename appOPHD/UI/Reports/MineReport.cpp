6760d2da25a29651343d6a93f62ee6f5a35d30ae
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-02-25 02:15:12 -0700

    Position progress bar using size of resource name line

--- appOPHD/UI/Reports/MineReport.cpp
+++ appOPHD/UI/Reports/MineReport.cpp
@@ -399,7 +399,8 @@ void MineReport::drawOreProductionPane(const NAS2D::Point<int>& origin)
 		const auto resourceNameOffset = NAS2D::Vector{ResourceImageRectsOre[i].size.x + constants::MarginTight + 2, 0};
 		renderer.drawText(fontBold, ResourceNamesOre[i], resourceIconPosition + resourceNameOffset, constants::PrimaryTextColor);
 
-		const auto progressBarPosition = resourceIconPosition + NAS2D::Vector{0, 20};
+		const auto resourceNameHeight = std::max({ResourceImageRectsOre[i].size.y, fontBold.height(), chkResources[i].size().y});
+		const auto progressBarPosition = resourceIconPosition + NAS2D::Vector{0, resourceNameHeight + constants::MarginTight + 2};
 		const auto progressBarArea = NAS2D::Rectangle{progressBarPosition, progressBarSize};
 		drawProgressBar(
 			oreAvailable.resources[i],
