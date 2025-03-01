cf66f16946dc52479cb5785582497128d76cc40f
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-03-01 10:26:13 -0700

    Add layout helpers `setPositionX` and `setPositionY`

--- libControls/Layout.cpp
+++ libControls/Layout.cpp
@@ -25,3 +25,21 @@ void setSize(const std::vector<Control*>& controls, NAS2D::Vector<int> newSize)
 		control->size(newSize);
 	}
 }
+
+
+void setPositionX(const std::vector<Control*>& controls, int newX)
+{
+	for (auto* control : controls)
+	{
+		control->position({newX, control->position().y});
+	}
+}
+
+
+void setPositionY(const std::vector<Control*>& controls, int newY)
+{
+	for (auto* control : controls)
+	{
+		control->position({control->position().x, newY});
+	}
+}
--- libControls/Layout.h
+++ libControls/Layout.h
@@ -15,3 +15,6 @@ class Control;
 
 NAS2D::Vector<int> maxSize(const std::vector<Control*>& controls);
 void setSize(const std::vector<Control*>& controls, NAS2D::Vector<int> newSize);
+
+void setPositionX(const std::vector<Control*>& controls, int newX);
+void setPositionY(const std::vector<Control*>& controls, int newY);
