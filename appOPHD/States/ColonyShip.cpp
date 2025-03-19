08da4bca5fa9980e336ac4e8acd744e80a9d5840
Alexander Hunt <alexander.l.hunt951@gmail.com>
2025-03-19 08:50:10 -0400

    Added colonyShipDataFromSave

--- appOPHD/States/ColonyShip.cpp
+++ appOPHD/States/ColonyShip.cpp
@@ -2,6 +2,47 @@
 
 #include "appOPHD/Constants/Strings.h"
 
+#include <NAS2D/ParserHelper.h>
+
+namespace
+{
+	void setLanders(ColonyShipData& colonyShipData, NAS2D::Xml::XmlElement* element)
+	{
+		if (element)
+		{
+			const auto dictionary = NAS2D::attributesToDictionary(*element);
+			colonyShipData.colonistLanders = dictionary.get<int>("colonist_landers");
+			colonyShipData.cargoLanders = dictionary.get<int>("cargo_landers");
+		}
+	}
+
+
+	void setManeuveringFuel(ColonyShipData& colonyShipData, NAS2D::Xml::XmlElement* element)
+	{
+		if (element)
+		{
+			auto turnCount = NAS2D::attributesToDictionary(*element).get<int>("count");
+
+			colonyShipData.turnsOfManeuveringFuel = turnCount > constants::ColonyShipOrbitTime ? 0 : constants::ColonyShipOrbitTime - turnCount + 1;
+		}
+	}
+}
+
+
+ColonyShipData colonyShipDataFromSave(NAS2D::Xml::XmlDocument& xmlDocument)
+{
+	auto* root = xmlDocument.firstChildElement(constants::SaveGameRootNode);
+	if (root)
+	{
+		ColonyShipData colonyShipData;
+		setLanders(colonyShipData, root->firstChildElement("population"));
+		setManeuveringFuel(colonyShipData, root->firstChildElement("turns"));
+		return colonyShipData;
+	}
+	throw std::runtime_error("Invalid save game root element.");
+}
+
+
 ColonyShip::ColonyShip()
 {}
 
