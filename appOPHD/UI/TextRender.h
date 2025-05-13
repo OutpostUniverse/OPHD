b443081c401c62f7d2a220862d998c0764399a35
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-05-13 10:59:23 -0600

    Add helper method `drawLabelRightJustify`

--- appOPHD/UI/TextRender.h
+++ appOPHD/UI/TextRender.h
@@ -6,6 +6,14 @@
 #include <string>
 
 
+namespace NAS2D
+{
+	class Font;
+}
+
+
+void drawLabelRightJustify(NAS2D::Point<int> position, int labelWidth, const NAS2D::Font& font, const std::string& text, NAS2D::Color color = NAS2D::Color::White);
+
 void drawLabelAndValue(NAS2D::Point<int> position, const std::string& title, const std::string& text, NAS2D::Color color = NAS2D::Color::White);
 void drawLabelAndValueLeftJustify(NAS2D::Point<int> position, int labelWidth, const std::string& title, const std::string& text, NAS2D::Color color = NAS2D::Color::White);
 void drawLabelAndValueRightJustify(NAS2D::Point<int> position, int labelWidth, const std::string& title, const std::string& text, NAS2D::Color color = NAS2D::Color::White);
