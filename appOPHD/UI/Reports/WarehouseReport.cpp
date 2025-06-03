6b291f7cd65028336064581bcb047a530f2389d2
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-06-03 10:01:34 -0600

    Use `lstStructures.isItemSelected()` for clarity

--- appOPHD/UI/Reports/WarehouseReport.cpp
+++ appOPHD/UI/Reports/WarehouseReport.cpp
@@ -233,7 +233,7 @@ void WarehouseReport::onResize()
 
 void WarehouseReport::onVisibilityChange(bool /*visible*/)
 {
-	btnTakeMeThere.visible(selectedWarehouse() != nullptr);
+	btnTakeMeThere.visible(lstStructures.isItemSelected());
 }
 
 
