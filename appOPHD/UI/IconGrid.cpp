a7c4de50282ddba997ef0a94717b52ada6e041e0
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-06-08 14:11:27 -0600

    Remove duplicate `visibile` check

--- appOPHD/UI/IconGrid.cpp
+++ appOPHD/UI/IconGrid.cpp
@@ -298,11 +298,6 @@ void IconGrid::onMouseDown(MouseButton button, NAS2D::Point<int> position)
 	// Don't respond to anything unless it's the left mouse button.
 	if (button != MouseButton::Left) { return; }
 
-	if (!visible())
-	{
-		return;
-	}
-
 	auto startPoint = mRect.position;
 	if (mIconItemList.empty() || !NAS2D::Rectangle{startPoint, mGridSizeInIcons * (mIconSize + mIconMargin)}.contains(position))
 	{
