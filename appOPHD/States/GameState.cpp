6391329d86ef95fc3d01bc0633fd2056b9b83842
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-06-08 16:06:06 -0600

    Add missing include for `UiConstants.h` to `GameState.cpp`

--- appOPHD/States/GameState.cpp
+++ appOPHD/States/GameState.cpp
@@ -6,6 +6,7 @@
 #include "../StructureManager.h"
 #include "../UI/MessageBox.h"
 #include "../Constants/Strings.h"
+#include "../Constants/UiConstants.h"
 
 #include <NAS2D/Filesystem.h>
 #include <NAS2D/Utility.h>
