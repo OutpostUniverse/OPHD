376d276583da2fae3d31b6409d909899ed50c5b2
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-06-10 21:40:37 -0600

    Update size and text

--- demoLibControls/MainWindow.cpp
+++ demoLibControls/MainWindow.cpp
@@ -7,11 +7,12 @@
 MainWindow::MainWindow() :
 	Window{"Main Window"},
 	button{"Button", {this, &MainWindow::onButtonClick}},
-	label{"Label"}
+	label{"Label: Waiting for button"}
 {
 	const auto& renderer = NAS2D::Utility<NAS2D::Renderer>::get();
 	size(renderer.size());
 
+	button.size(button.size() + NAS2D::Vector{8, 2});
 	add(button, {10, 30});
 	add(label, {70, 30});
 }
@@ -19,5 +20,5 @@ MainWindow::MainWindow() :
 
 void MainWindow::onButtonClick()
 {
-	label.text("Button was clicked");
+	label.text("Label: Button was clicked");
 }
