cf6a99ab02b0f3d06c995787983457b3677d1c9f
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-06-10 21:20:40 -0600

    Add `Font` and `Image` cache

--- demoLibControls/main.cpp
+++ demoLibControls/main.cpp
@@ -1,6 +1,10 @@
+#include "Cache.h"
 #include "MainWindowState.h"
 
+#include <libControls/Control.h>
+
 #include <NAS2D/Game.h>
+#include <NAS2D/Resource/ResourceCache.h>
 
 #include <iostream>
 
@@ -10,6 +14,11 @@ int main()
 	try
 	{
 		NAS2D::Game game{"Demo libControls", "DemoLibControls", "LairWorks"};
+
+		Control::setDefaultFont(fontCache.load("fonts/opensans.ttf", 14));
+		Control::setDefaultFontBold(fontCache.load("fonts/opensans-bold.ttf", 14));
+		Control::setImageCache(imageCache);
+
 		game.go(new MainWindowState);
 	}
 	catch (const std::exception& exception)
