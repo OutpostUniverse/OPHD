f4550d7e05e8d78d621233d23e0e2b9ad56d2fa5
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-06-22 10:58:29 -0600

    Remove "UI" from comments
    It seemed a bit redundant to include it, plus it was removed from the `MainReportsUiState` name.

--- appOPHD/UI/Reports/Report.h
+++ appOPHD/UI/Reports/Report.h
@@ -7,7 +7,7 @@ class Structure;
 
 
 /**
- * Provides an abstract interface for Report UIs used in the ReportsState.
+ * Provides an abstract interface for Report used in the ReportsState.
  *
  * \note	Inherits from ControlContainer as the report classes are treated as Control objects.
  */
@@ -17,24 +17,24 @@ public:
 	using ControlContainer::update;
 
 	/**
-	 * Instructs the Report UI to clear any selections it may have.
+	 * Instructs the Report to clear any selections it may have.
 	 */
 	virtual void clearSelected() = 0;
 
 	/**
-	 * Instructs the Report UI that it should fill any lists it needs to (the
+	 * Instructs the Report that it should fill any lists it needs to (the
 	 * panel is coming into focus).
 	 */
 	virtual void fillLists() = 0;
 
 	/**
-	 * Instructs the Report UI to refresh itself. Used to clear any state
+	 * Instructs the Report to refresh itself. Used to clear any state
 	 * information to a 'like new' state.
 	 */
 	virtual void refresh() = 0;
 
 	/**
-	 * Instructs a Report UI to set its primary selection to a specified Structure.
+	 * Instructs a Report to set its primary selection to a specified Structure.
 	 *
 	 * \note	Casting may be necessary depending on implementation of inheriting
 	 *			classes. Be mindful to pass pointers to objects that can be safely
