d50ef99688c5a0d6eb75912c77bb605cc5526241
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-06-30 00:11:44 -0600

    Set size before position in `ListBoxBase`

--- libControls/ListBoxBase.cpp
+++ libControls/ListBoxBase.cpp
@@ -112,8 +112,8 @@ void ListBoxBase::updateScrollLayout()
 
 	if ((mItemSize.y * static_cast<int>(count())) > mRect.size.y)
 	{
-		mScrollBar.position({area().position.x + mRect.size.x - 14, mRect.position.y});
 		mScrollBar.size({14, mRect.size.y});
+		mScrollBar.position({area().position.x + mRect.size.x - 14, mRect.position.y});
 		mScrollBar.max(static_cast<ScrollBar::ValueType>(mItemSize.y * static_cast<int>(count()) - mRect.size.y));
 		mScrollOffsetInPixels = mScrollBar.value();
 		mItemSize.x -= mScrollBar.size().x;
