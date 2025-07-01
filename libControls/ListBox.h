ce1b80b11aada2ccdc888708f7caaf36bcffaf85
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-06-30 18:53:56 -0600

    Add early return for `isEmpty()` and middle-click
    There is no mouse processing that can be done for an empty `ListBox`. Additionally we don't wish to select items using middle-click, or otherwise process middle-click.

--- libControls/ListBox.h
+++ libControls/ListBox.h
@@ -2,6 +2,8 @@
 
 #include "Control.h"
 #include "ScrollBar.h"
+
+#include <NAS2D/EnumMouseButton.h>
 #include <NAS2D/Utility.h>
 #include <NAS2D/Signal/Delegate.h>
 #include <NAS2D/EventHandler.h>
@@ -222,9 +224,10 @@ protected:
 	}
 
 
-	virtual void onMouseDown(NAS2D::MouseButton /*button*/, NAS2D::Point<int> position)
+	virtual void onMouseDown(NAS2D::MouseButton button, NAS2D::Point<int> position)
 	{
 		if (!visible() || !enabled() || !mRect.contains(position)) { return; }
+		if (isEmpty() || button == NAS2D::MouseButton::Middle) { return; }
 
 		if (mHighlightIndex == NoSelection || !mScrollArea.contains(position)) { return; }
 		setSelected(mHighlightIndex);
