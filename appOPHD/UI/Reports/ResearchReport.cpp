93d5eb1b53277ae4ea1b05a9be35822f02861981
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-07-02 03:32:37 -0600

    Move NAS2D includes below local project includes in `ResearchReport.cpp`

--- appOPHD/UI/Reports/ResearchReport.cpp
+++ appOPHD/UI/Reports/ResearchReport.cpp
@@ -1,10 +1,5 @@
 #include "ResearchReport.h"
 
-#include <NAS2D/Utility.h>
-#include <NAS2D/EventHandler.h>
-#include <NAS2D/EnumMouseButton.h>
-#include <NAS2D/Renderer/Renderer.h>
-
 #include "../../Constants/Strings.h"
 #include "../../Constants/UiConstants.h"
 #include "../../Cache.h"
@@ -12,6 +7,11 @@
 
 #include "../../MapObjects/Structures/HotLaboratory.h"
 #include "../../MapObjects/Structures/Laboratory.h"
+
+#include <NAS2D/Utility.h>
+#include <NAS2D/EventHandler.h>
+#include <NAS2D/EnumMouseButton.h>
+#include <NAS2D/Renderer/Renderer.h>
 
 #include <array>
 #include <vector>
