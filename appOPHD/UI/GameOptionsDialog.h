54291764a68e8dc4df4df5aaca3b866965c9ab4e
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-07-09 07:02:21 -0600

    Add `Escape` key handling to close `GameOptions` window

--- appOPHD/UI/GameOptionsDialog.h
+++ appOPHD/UI/GameOptionsDialog.h
@@ -3,6 +3,15 @@
 #include <libControls/Window.h>
 #include <libControls/Button.h>
 
+#include <cstdint>
+
+
+namespace NAS2D
+{
+	enum class KeyCode : uint32_t;
+	enum class KeyModifier : uint16_t;
+}
+
 
 class GameOptionsDialog : public Window
 {
@@ -15,13 +24,16 @@ public:
 		ClickHandler exitClickHandler,
 		ClickHandler continueClickHandler
 	);
+	~GameOptionsDialog() override;
 
 protected:
+	void onKeyDown(NAS2D::KeyCode key, NAS2D::KeyModifier mod, bool repeat);
 	void onHelp();
 
 	void onEnableChange() override;
 
 private:
+	ClickHandler mContinueClickHandler;
 	Button btnSave;
 	Button btnLoad;
 	Button btnHelp;
