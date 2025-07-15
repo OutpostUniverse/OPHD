262a87a285d3ef0440887858f001f7866369536e
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-07-14 19:02:45 -0600

    Remove no longer needed clamping in thumb size calculation
    Since we have clamped minimums for `mMax` (`0`), and `mLargeDelta` (`1`), we know their sum is at least `1`, and so there is no danger of division by `0`.

--- libControls/ScrollBar.cpp
+++ libControls/ScrollBar.cpp
@@ -258,13 +258,13 @@ void ScrollBar::onThumbResize()
 {
 	if (mScrollBarType == ScrollBarType::Vertical)
 	{
-		const auto naturalThumbLength = mTrackRect.size.y * mLargeDelta / std::max(mMax + mLargeDelta, 1);
+		const auto naturalThumbLength = mTrackRect.size.y * mLargeDelta / (mMax + mLargeDelta);
 		const auto thumbLength = std::clamp(naturalThumbLength, mSkins.skinThumb.minSize().y, mTrackRect.size.y);
 		mThumbRect.size = {mTrackRect.size.x, thumbLength};
 	}
 	else
 	{
-		const auto naturalThumbLength = mTrackRect.size.x * mLargeDelta / std::max(mMax + mLargeDelta, 1);
+		const auto naturalThumbLength = mTrackRect.size.x * mLargeDelta / (mMax + mLargeDelta);
 		const auto thumbLength = std::clamp(naturalThumbLength, mSkins.skinThumb.minSize().x, mTrackRect.size.x);
 		mThumbRect.size = {thumbLength, mTrackRect.size.y};
 	}
