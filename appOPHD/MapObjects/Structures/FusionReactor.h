9f5ba70e03dbe2f53c756d5f5046ecdda785ec1a
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-07-21 15:45:29 -0600

    Use default implementation for non-solar power

--- appOPHD/MapObjects/Structures/FusionReactor.h
+++ appOPHD/MapObjects/Structures/FusionReactor.h
@@ -3,20 +3,11 @@
 #include "PowerStructure.h"
 
 
-constexpr int FusionReactorBaseProduction = 1000;
-
-
 class FusionReactor : public PowerStructure
 {
 public:
 	FusionReactor(Tile& tile) :
 		PowerStructure{StructureID::SID_FUSION_REACTOR, tile}
 	{
-	}
-
-protected:
-	int calculateMaxEnergyProduction() const override
-	{
-		return FusionReactorBaseProduction;
 	}
 };
--- appOPHD/MapObjects/Structures/SeedPower.h
+++ appOPHD/MapObjects/Structures/SeedPower.h
@@ -3,19 +3,11 @@
 #include "PowerStructure.h"
 
 
-const int SeedPowerProduction = 50;
-
 class SeedPower : public PowerStructure
 {
 public:
 	SeedPower(Tile& tile) :
 		PowerStructure{StructureID::SID_SEED_POWER, tile}
 	{
-	}
-
-private:
-	int calculateMaxEnergyProduction() const override
-	{
-		return SeedPowerProduction;
 	}
 };
