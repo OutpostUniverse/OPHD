7318be9b9f6e8d09d15f95acfad185c30d1d4561
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-07-29 05:46:01 -0600

    Extract common expression to `refinedOreCapacity`

--- appOPHD/UI/ResourceInfoBar.cpp
+++ appOPHD/UI/ResourceInfoBar.cpp
@@ -184,9 +184,10 @@ void ResourceInfoBar::draw(NAS2D::Renderer& renderer) const
 
 	// Capacity (Storage, Food, Energy)
 	const auto& sm = NAS2D::Utility<StructureManager>::get();
+	const auto refinedOreCapacity = totalStorage(StructureClass::Storage, 1000);
 	const std::array storageCapacities
 	{
-		std::tuple{NAS2D::Rectangle<int>{{96, 32}, {iconSize, iconSize}}, mResourcesCount.total(), totalStorage(StructureClass::Storage, 1000), totalStorage(StructureClass::Storage, 1000) - mResourcesCount.total() <= 100},
+		std::tuple{NAS2D::Rectangle<int>{{96, 32}, {iconSize, iconSize}}, mResourcesCount.total(), refinedOreCapacity, refinedOreCapacity - mResourcesCount.total() <= 100},
 		std::tuple{NAS2D::Rectangle<int>{{64, 32}, {iconSize, iconSize}}, mFood, sm.totalFoodStorageCapacity(), mFood <= 10},
 		std::tuple{NAS2D::Rectangle<int>{{80, 32}, {iconSize, iconSize}}, sm.totalEnergyAvailable(), sm.totalEnergyProduction(), sm.totalEnergyAvailable() <= 5}
 	};
