49317fdb53a66e9a2dd120051537b24fe31f2fc7
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-08-12 02:44:22 -0600

    Update `classDescription` to return `mStructureType.description`

--- appOPHD/MapObjects/Structure.cpp
+++ appOPHD/MapObjects/Structure.cpp
@@ -33,38 +33,6 @@ const std::map<StructureState, std::string> StructureStateDescriptions =
 };
 
 
-const std::map<StructureClass, std::string> StructureClassNames =
-{
-	{StructureClass::Command, "Command"},
-	{StructureClass::Communication, "Communication"},
-	{StructureClass::Commercial, "Commercial"},
-	{StructureClass::EnergyProduction, "Energy Production"},
-	{StructureClass::Factory, "Factory"},
-	{StructureClass::FoodProduction, "Food Production"},
-	{StructureClass::Laboratory, "Laboratory"},
-	{StructureClass::Lander, "Lander"},
-	{StructureClass::LifeSupport, "Life Support"},
-	{StructureClass::Maintenance, "Maintenance Facility"},
-	{StructureClass::Mine, "Mine Facility"},
-	{StructureClass::MedicalCenter, "Medical Center"},
-	{StructureClass::Nursery, "Nursery"},
-	{StructureClass::Park, "Park / Reservoir"},
-	{StructureClass::Road, "Road"},
-	{StructureClass::SurfacePolice, "Police"},
-	{StructureClass::UndergroundPolice, "Police"},
-	{StructureClass::RecreationCenter, "Recreation Center"},
-	{StructureClass::Recycling, "Recycling"},
-	{StructureClass::Residence, "Residential"},
-	{StructureClass::RobotCommand, "Robot Command Center"},
-	{StructureClass::Smelter, "Raw Ore Processing"},
-	{StructureClass::Storage, "Storage"},
-	{StructureClass::Tube, "Tube"},
-	{StructureClass::Undefined, "UNDEFINED"},
-	{StructureClass::University, "University"},
-	{StructureClass::Warehouse, "Warehouse"}
-};
-
-
 Structure::Structure(StructureID id, Tile& tile) :
 	Structure{id, tile, constants::StructureStateConstruction}
 {
@@ -239,12 +207,7 @@ const std::string& Structure::stateDescription(StructureState state)
 
 const std::string& Structure::classDescription() const
 {
-	return classDescription(mStructureClass);
-}
-
-const std::string& Structure::classDescription(StructureClass structureClass)
-{
-	return StructureClassNames.at(structureClass);
+	return mStructureType.description;
 }
 
 int Structure::turnsToBuild() const
