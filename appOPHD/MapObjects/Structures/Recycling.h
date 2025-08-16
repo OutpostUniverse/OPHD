fd5b80ab3ba222897d22c0360542144534e50c85
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-08-16 03:46:09 -0600

    Remove unnecessary `virtual` from `Recycling` methods

--- appOPHD/MapObjects/Structures/Recycling.h
+++ appOPHD/MapObjects/Structures/Recycling.h
@@ -8,8 +8,8 @@ class Recycling : public Structure
 public:
 	Recycling(Tile& tile);
 
-	virtual int wasteProcessingCapacity() const;
-	virtual int residentialSupportCount() const;
+	int wasteProcessingCapacity() const;
+	int residentialSupportCount() const;
 
 	StringTable createInspectorViewTable() const override;
 };
