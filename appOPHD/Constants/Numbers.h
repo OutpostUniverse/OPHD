d901038c4c26bfab6a491af2c1c5e011a31e92e1
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-08-24 23:19:52 -0600

    Remove unused constant `BaseStorageCapacity`

--- appOPHD/Constants/Numbers.h
+++ appOPHD/Constants/Numbers.h
@@ -8,8 +8,6 @@ namespace constants
 {
 	inline constexpr int SmeltingMinimumResourcesCount{20};
 
-	inline constexpr int BaseStorageCapacity{250};
-
 	inline constexpr int BaseProductCapacity{100};
 
 	inline constexpr int BaseMineProductionRate{10};
