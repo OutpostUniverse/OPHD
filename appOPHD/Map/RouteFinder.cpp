0febabb7b16b4771dc8b894e14a2129f51f0d3ab
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-09-01 02:51:31 -0600

    Group terrain checks together and structure checks together

--- appOPHD/Map/RouteFinder.cpp
+++ appOPHD/Map/RouteFinder.cpp
@@ -59,11 +59,6 @@ namespace
 
 	float tileMovementCost(const Tile& tile)
 	{
-		if (tile.index() == TerrainType::Impassable)
-		{
-			return FLT_MAX;
-		}
-
 		if (tile.hasStructure() && tile.structure()->isRoad())
 		{
 			Structure& road = *tile.structure();
@@ -83,6 +78,11 @@ namespace
 		}
 
 		if (tile.hasMapObject() && (!tile.hasStructure() || (!tile.structure()->isMineFacility() && !tile.structure()->isSmelter())))
+		{
+			return FLT_MAX;
+		}
+
+		if (tile.index() == TerrainType::Impassable)
 		{
 			return FLT_MAX;
 		}
