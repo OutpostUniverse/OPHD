7d78bddd6fddb4f27baf6c725e3d0147af5d8e68
Daniel Stevens <Dan.R.Stevens@gmail.com>
2025-09-13 00:17:37 -0600

    Move `Font` related constants to `CacheFont`

--- appOPHD/CacheFont.cpp
+++ appOPHD/CacheFont.cpp
@@ -8,16 +8,23 @@
 
 namespace
 {
+	const std::string FontPrimary = "fonts/opensans.ttf";
+	const std::string FontPrimaryBold = "fonts/opensans-bold.ttf";
+
+	constexpr int FontPrimaryNormal{14};
+	constexpr int FontPrimaryMedium{18};
+	constexpr int FontPrimaryHuge{24};
+
 	NAS2D::ResourceCache<NAS2D::Font, std::string, unsigned int> fontCache;
 }
 
 
-const NAS2D::Font& getFont(unsigned int size) { return fontCache.load(constants::FontPrimary, size); }
-const NAS2D::Font& getFontBold(unsigned int size) { return fontCache.load(constants::FontPrimaryBold, size); }
+const NAS2D::Font& getFont(unsigned int size) { return fontCache.load(FontPrimary, size); }
+const NAS2D::Font& getFontBold(unsigned int size) { return fontCache.load(FontPrimaryBold, size); }
 
-const NAS2D::Font& getFont() { return getFont(constants::FontPrimaryNormal); }
-const NAS2D::Font& getFontMedium() { return getFont(constants::FontPrimaryMedium); }
+const NAS2D::Font& getFont() { return getFont(FontPrimaryNormal); }
+const NAS2D::Font& getFontMedium() { return getFont(FontPrimaryMedium); }
 
-const NAS2D::Font& getFontBold() { return getFontBold(constants::FontPrimaryNormal); }
-const NAS2D::Font& getFontMediumBold() { return getFontBold(constants::FontPrimaryMedium); }
-const NAS2D::Font& getFontHugeBold() { return getFontBold(constants::FontPrimaryHuge); }
+const NAS2D::Font& getFontBold() { return getFontBold(FontPrimaryNormal); }
+const NAS2D::Font& getFontMediumBold() { return getFontBold(FontPrimaryMedium); }
+const NAS2D::Font& getFontHugeBold() { return getFontBold(FontPrimaryHuge); }
